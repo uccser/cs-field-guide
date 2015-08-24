f7a4aaa23df4946184f018c6110ef0777b8aca3f
Hayley van Waas <hayleyavw@gmail.com>
2015-08-24 15:21:14 +1200

    Added license file for use of CryptoJS

--- interactives/MD5-hash/md5-hash-script.js
+++ interactives/MD5-hash/md5-hash-script.js
@@ -7,8 +7,8 @@
 
 $(document).ready(function () {
     $("#interactive-md5hash-hash").click(function(){
-        console.log("hi");
-        var hashed_message = CryptoJS.MD5("Message").toString();
+        var text = $('#interactive-md5hash-text-input').val();
+        var hashed_message = CryptoJS.MD5(text).toString();
         document.getElementById('interactive-md5hash-hashed-message').innerHTML = hashed_message;
     });
 });
