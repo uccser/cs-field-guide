5666a66cf1d1521a2776349f664107c56a9295d9
Jack Morgan <jackmorgannz@gmail.com>
2016-02-26 10:40:42 +1300

    Finished up word filter interactive (fixes #173)

--- interactives/regular-expression-filter/js/regular-expression-filter.js
+++ interactives/regular-expression-filter/js/regular-expression-filter.js
@@ -1,4 +1,5 @@
 RegularExpressionFilter = {};
+RegularExpressionFilter.regex = new RegExp('');
 
 $(document).ready(function () {
 
@@ -10,58 +11,62 @@ $(document).ready(function () {
     updateWordCount();
   });
 
-
+  // Read file once page loaded
   readWords();
-  RegularExpressionFilter.regex = new RegExp('');
 });
 
-function fileLoaded () {
-  document.getElementById('interactive-regular-expression-filter-words').style.display = 'none';
-  document.getElementById('interactive-regular-expression-filter-loading-progress').style.display = 'none';
-  document.getElementById('interactive-regular-expression-filter-processing-progress').style.display = 'block';
-  RegularExpressionFilter.words = this.responseText.split('\n');
-  displayWords();
-  updateWordCount();
-  document.getElementById('interactive-regular-expression-filter-processing-progress').style.display = 'none';
-  document.getElementById('interactive-regular-expression-filter-words').style.display = 'block';
+
+// Displays the filtered words
+function displayWords() {
+  RegularExpressionFilter.displayed = 0;
+  html = ""
+  var display = document.getElementById('interactive-regular-expression-filter-words');
+  for (var i = 0; i < RegularExpressionFilter.words.length; i++) {
+    if (RegularExpressionFilter.regex.test(RegularExpressionFilter.words[i])) {
+      html += RegularExpressionFilter.words[i] + '\n';
+      RegularExpressionFilter.displayed++;
+    }
+  }
+  display.innerHTML = html.trim();
 };
 
+
+// Updates the word count
+function updateWordCount() {
+  var text = "Showing " + RegularExpressionFilter.displayed + " out of " + RegularExpressionFilter.words.length + " lower case words"
+  document.getElementById('interactive-regular-expression-filter-feedback').innerHTML = text;
+};
+
+
+// Reads the words from the file
 function readWords() {
   document.getElementById('interactive-regular-expression-filter-loading-progress').style.display = 'block';
   var request = new XMLHttpRequest();
   request.addEventListener('load', fileLoaded);
   request.addEventListener('progress', updateLoadingProgress);
+  request.addEventListener('error', function() { alert('This interactive must be viewed online to load cross origin text file. Sorry for the inconvenience.')});
   request.open('get', 'en-words.txt');
   request.send();
 };
 
-function displayWords() {
-  var progress_bar = document.getElementById('interactive-regular-expression-filter-processing-progress-bar').style.width = '0%';
-  RegularExpressionFilter.displayed = 0;
-  text = ""
-  var display = document.getElementById('interactive-regular-expression-filter-words');
-  var progress_bar = document.getElementById('interactive-regular-expression-filter-processing-progress-bar')
-  for (var i = 0; i < RegularExpressionFilter.words.length; i++) {
-    if (RegularExpressionFilter.regex.test(RegularExpressionFilter.words[i])) {
-      text += RegularExpressionFilter.words[i];
-      RegularExpressionFilter.displayed++;
-    }
-    progress_bar.style.width = ((i / RegularExpressionFilter.words.length) * 100) + '%';
-  }
-  display.innerHTML = text;
-};
 
-
-function updateWordCount() {
-  var text = "Showing " + RegularExpressionFilter.displayed + " out of " + RegularExpressionFilter.words.length + " words"
-  document.getElementById('interactive-regular-expression-filter-word-count').innerHTML = text;
-};
-
-
+// Updates the file loading bar
 function updateLoadingProgress(request) {
   if (request.lengthComputable) {
     // Calculates percent complete (as a whole number e.g. 80%)
     var completed = (request.loaded / request.total) * 100;
     document.getElementById('interactive-regular-expression-filter-loading-progress-bar').style.width = completed + '%';
   }
+};
+
+
+// This function is triggered when the file is finished loading
+function fileLoaded () {
+  document.getElementById('interactive-regular-expression-filter-words').style.display = 'none';
+  document.getElementById('interactive-regular-expression-filter-loading-progress').style.display = 'none';
+  RegularExpressionFilter.words = this.responseText.split('\n');
+  displayWords();
+  updateWordCount();
+  document.getElementById('interactive-regular-expression-filter-words').style.display = 'block';
+  document.getElementById('interactive-regular-expression-filter-controls').style.display = 'block';
 };
