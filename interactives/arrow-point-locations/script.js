70f3be58bcfe892bf6f6ff8d6f969f7a3e6ef1f6
Courtney Bracefield <courtneybracefield86@gmail.com>
2016-03-02 15:21:50 +1300

    Added a new library for experimentation. Removed grid.

--- interactives/arrow-point-locations/script.js
+++ interactives/arrow-point-locations/script.js
@@ -1,61 +1,26 @@
-$(document).ready(function(){
-	grid('blocks', 20, 20, 700, 500);
-});
-
-function grid(type, w, h, totalW, totalH){
-
-    var $this = this;
-    this.type = type || 'blocks'; // blocks, diamonds, hexagons
-    this.blockW = w || 25;
-    this.blockH = h || 25;
-    this.container;
-        $('#grid').empty();
-  
-    this.container = document.createElement('div');
-    this.container.style.position = 'absolute';
-    this.container.style.width = '100%';
-    this.container.style.height = '100%';
-    this.container.id = 'gridContainer';
-        
-  
-    var c = document.createElement("canvas");
-        c.width  = totalW;
-        c.height = totalH;
-    
-    var totalW = totalW || $(document).width();
-    var totalH = totalH || $(document).height();
-
-        
-    var mapGridCanvas = c.getContext("2d");
-    mapGridCanvas.clearRect(0, 0, c.width, c.height);
-    mapGridCanvas.globalAlpha = 1;
-    mapGridCanvas.strokeStyle = "#1e1e1e";
-    mapGridCanvas.lineWidth = 1;
-        mapGridCanvas.beginPath();
-        var x = 0;
-        var y = 0;
-    
-    for(var i = 0; i < Math.round(totalW / blockW); i++){
-                
-        x = i * blockW;
-        y = 0;
-        mapGridCanvas.moveTo(x, y);
-        mapGridCanvas.lineTo(x, y + totalH);
-        
+function onLoad() {
+    var graph = Viva.Graph.graph(),
+        nodePositions = [{x : -50, y: 0}, {x : 0, y: -50}, {x : 50, y: 0}], // predefined node positions
+        layout = Viva.Graph.Layout.constant(graph),
+        renderer = Viva.Graph.View.renderer(graph, {
+                       layout     : layout, // use our custom 'constant' layout
+                   }),
+        i, nodesCount = nodePositions.length; // convinience variables.
+    // Add nodes
+    for(i = 0; i < nodesCount; ++i) {
+        graph.addNode(i, nodePositions[i]);
     }
-        
-    for(var j = 0; j < Math.round(totalH / blockH); j++){
-                
-        x = 0;
-        y = j * blockH;
-                
-        mapGridCanvas.moveTo(x, y);
-        mapGridCanvas.lineTo(x + totalW, y);   
-                
+    // and make them connected in cycle:
+    for (i = 0; i < nodesCount; ++i) {
+        graph.addLink(i % nodesCount, (i + 1) % nodesCount);
     }
-        
-    mapGridCanvas.stroke();
-    this.container.appendChild(c);
-    document.getElementById('grid').appendChild(this.container);
-        
+    // set custom node placement callback for layout.
+    // if you don't do this, constant layout performs random positioning.
+    layout.placeNode(function(node) {
+        // node.id - points to its position but you can do your
+        // random logic here. E.g. read from specific node.data
+        // attributes. This callback is expected to return object {x : .. , y : .. }
+        return nodePositions[node.id];
+    });
+    renderer.run();
 }
