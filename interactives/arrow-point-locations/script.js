52d97932c97b1508d3098cb851826e25a79dcd8c
Courtney Bracefield <courtneybracefield86@gmail.com>
2016-03-07 13:18:19 +1300

    Added more nodes and connected them into an arrow shape.

--- interactives/arrow-point-locations/script.js
+++ interactives/arrow-point-locations/script.js
@@ -1,6 +1,7 @@
 function onLoad() {
     var graph = Viva.Graph.graph(),
-        nodePositions = [{x : -50, y: 0}, {x : 0, y: -50}, {x : 50, y: 0}], // predefined node positions
+        nodePositions = [{x : -80, y: 0}, {x : 0, y: -80}, {x : 80, y: 0}, {x : -25, y: 0}, {x : 25, y: 0},
+        {x : -25, y : 150}, {x : 25, y : 150}], // predefined node positions
         layout = Viva.Graph.Layout.constant(graph),
         renderer = Viva.Graph.View.renderer(graph, {
                        layout     : layout, // use our custom 'constant' layout
@@ -10,10 +11,16 @@ function onLoad() {
     for(i = 0; i < nodesCount; ++i) {
         graph.addNode(i, nodePositions[i]);
     }
-    // and make them connected in cycle:
-    for (i = 0; i < nodesCount; ++i) {
-        graph.addLink(i % nodesCount, (i + 1) % nodesCount);
-    }
+    // and make them connected 
+
+    graph.addLink(0,1);
+    graph.addLink(1,2);
+    graph.addLink(2,4);
+    graph.addLink(4,6);
+    graph.addLink(6,5);
+    graph.addLink(5,3);
+    graph.addLink(3,0);
+    
     // set custom node placement callback for layout.
     // if you don't do this, constant layout performs random positioning.
     layout.placeNode(function(node) {
