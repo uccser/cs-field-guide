e8c7b76aeb5106de2165f010bce2ffd942d69455
Hayley van Waas <hayleyavw@gmail.com>
2016-03-21 07:46:50 +1300

    Put symbols on each side of box, and removed unused code

--- interactives/box-translation/script.js
+++ interactives/box-translation/script.js
@@ -5,20 +5,14 @@ var container = document.getElementById( 'container' );
 var camera, scene, renderer;
 var cube, plane;
 var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 init();
-//createUserInputDiv();
 animate();
 
-function init() {
 
-    // container = document.createElement( 'div' );
-    // document.body.appendChild( container );
+function init() {
 
     var info = document.createElement( 'div' );
     info.style.position = 'absolute';
@@ -33,15 +27,37 @@ function init() {
 
     scene = new THREE.Scene();
 
-
-    // Cube
+    //Cube
 
     var geometry = new THREE.BoxGeometry( 200, 200, 200 );
 
+    var materials = [
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square1.jpg' )
+		}),
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square2.jpg' )
+		}),
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square3.jpg' )
+		}),
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square4.jpg' )
+		}),
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square5.jpg' )
+		}),
+		new THREE.MeshBasicMaterial({
+		   map: new THREE.TextureLoader().load( 'square6.jpg' )
+		})
+	];
+
     //for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+        //console.log(i);
 
         // random colour
         //var hex = Math.random() * 0xffffff;
+        //var hex = colours[i/2];
         //geometry.faces[ i ].color.setHex( hex );
         //geometry.faces[ i + 1 ].color.setHex( hex );
 
@@ -51,18 +67,19 @@ function init() {
 
     //}
 
-    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-
     // lego man
-    var texture = new THREE.TextureLoader().load( 'good_cop.jpg' );
-    var material = new THREE.MeshBasicMaterial( { map: texture } );
+    //var texture = new THREE.TextureLoader().load( 'good_cop.jpg' );
+    //var material = new THREE.MeshBasicMaterial( { map: texture } );
 
-    cube = new THREE.Mesh( geometry, material );
+    //var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
+
+    cube = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
     cube.position.y = 150;
     scene.add( cube );
 
 
     // Plane
+    // TODO: INVESTIGATE WHAT THIS IS
 
     var geometry = new THREE.PlaneBufferGeometry( 200, 200 );
     geometry.rotateX( - Math.PI / 2 );
@@ -82,12 +99,6 @@ function init() {
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
     container.appendChild( stats.domElement );
-
-    //document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    //document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    //document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-
-    //
 
     window.addEventListener( 'resize', onWindowResize, false );
 
@@ -118,71 +129,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-//
-
-//function onDocumentMouseDown( event ) {
-
-    //event.preventDefault();
-
-    //document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    //document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    //document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-
-    //mouseXOnMouseDown = event.clientX - windowHalfX;
-    //targetRotationOnMouseDown = targetRotation;
-
-//}
-
-//function onDocumentMouseMove( event ) {
-
-    //mouseX = event.clientX - windowHalfX;
-
-    //targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-
-//}
-
-function onDocumentMouseUp( event ) {
-
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-
-}
-
-function onDocumentMouseOut( event ) {
-
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-
-}
-
-function onDocumentTouchStart( event ) {
-
-    if ( event.touches.length === 1 ) {
-
-        event.preventDefault();
-
-        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotationOnMouseDown = targetRotation;
-
-    }
-
-}
-
-function onDocumentTouchMove( event ) {
-
-    if ( event.touches.length === 1 ) {
-
-        event.preventDefault();
-
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-
-    }
 
 }
 
