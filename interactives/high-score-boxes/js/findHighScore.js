2639c9a8754c176b64e6a2ca0bceab4ab31e96af
Victor Chang <thevictorchang@gmail.com>
2016-04-10 22:03:15 +1200

    New interactive: action-menu
    Very simple interactive, just a simple dropdown menu with
    one option that has severe consequences, but no confirmation screen
    if user selects that option. To demonstrate a key HCI concept.

--- interactives/high-score-boxes/js/findHighScore.js
+++ interactives/high-score-boxes/js/findHighScore.js
@@ -1,21 +1,32 @@
 //onload make array of 5 random ints
-
+//TODO: work out how to put number holding divs 'under' the box divs, then make them slide up
 var randomInts = [];
-var boxes = [];
 var largest = 0;
 var numberOfBoxes = 15
+var boxes = []
 
 $(document).ready(function(){
-	for (var i = 0; i < (numberOfBoxes); i++) {
-		var currentInt = getRandomInt(1, 100);
-		randomInts[i] = currentInt;
-	}
-	//assign largest of the 5 random ints to largest
-	largest = Math.max.apply(Math, randomInts);
+	generateRandomNumbers();
+	createBoxElements();
 	createBoxes();
+	createIntHoldingDivs();
 	console.log((randomInts.toString() + " " + largest.toString()));
 	console.log(boxes);
+
+	$(".box").click(function() {
+	console.log("box clicked!");
+	for (var i = 0; i < boxes.length; i++) {
+n		if (boxes[i].element === event.target) {
+			console.log("element matched!");
+			event.target.innerHTML = boxes[i].boxInt;
+			$("#int_holder_div" + i).slideUp("slow");
+						}	    
+	}
+
 })
+})
+
+
 /*
 function onLoadFunction() {
 	for (var i = 4; i >= 0; i--) {
@@ -29,20 +40,64 @@ function onLoadFunction() {
 	console.log(boxes);
 	}
 */
+function generateRandomNumbers() {
+		for (var i = 0; i < (numberOfBoxes); i++) {
+		var currentInt = getRandomInt(1, 100);
+		randomInts[i] = currentInt;
+	}
+	//assign largest of the 5 random ints to largest
+	largest = Math.max.apply(Math, randomInts);
+}
 
 function createBoxElements() {
+	console.log('we in createBoxElements!')
+		for (var i = 0; i < (numberOfBoxes); i++) {
 
+		var iDiv = document.createElement('div');
+		iDiv.id = ('box' + i);
+		iDiv.className = 'box';
+		//iDiv.innerHTML = 'REEEEEE';
+
+		document.getElementById('box_holder_div').appendChild(iDiv);
+	}
+}	
+
+function createIntHoldingDivs() {
+	for (var i = 0; i < (numberOfBoxes); i++) {
+		var intHoldingDiv = document.createElement('div');
+		intHoldingDiv.id = ("int_holder_div" + i);
+		intHoldingDiv.innerHTML = boxes[i].boxInt;
+		boxes[i].intHoldingDivElement = intHoldingDiv;
+		document.getElementById('box' + i).appendChild(intHoldingDiv);
+	}
 
 }
 function createBoxes() {
-	for (i = 0; i < numberOfBoxes; i++) {
+	for (var i = 0; i < numberOfBoxes; i++) {
 		var boxObject = {
 			boxNumber: i,
 			element: document.getElementById('box' + i),
-			boxInt: randomInts[i]
+			boxInt: randomInts[i],
+			intHoldingDivElement: null
 		}
 		boxes[i] = boxObject;
 	}
+}
+
+/*
+function addOnMouseClicks(boxes) {
+	for (var i = 0; i < boxes.length; i++) {
+		console.log(boxes[i].element)
+		boxes[i].element.onmouseover=function(){
+			console.log(boxes[i]);
+			boxes[i].element.innerHTML = boxes[i].boxInt;
+		}
+	}
+}
+*/
+
+function getBoxObjectFromBoxId(boxId) {
+	//TODO: this
 }
 
 function getRandomInt(min, max) {
