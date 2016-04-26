d3f7255e26f50565837a8cc12a81e53e65f25598
Hayley van Waas <hayleyavw@gmail.com>
2016-04-27 07:51:21 +1200

    Changed type of Tween for translation

--- interactives/box-translation-rotation/translation-script.js
+++ interactives/box-translation-rotation/translation-script.js
@@ -20,8 +20,8 @@ function moveBox() {
     // move the box on the next animation loop
     TWEEN.removeAll();
     new TWEEN.Tween( cube.position )
-        .to( target, 2000 )
-        .easing( TWEEN.Easing.Elastic.Out )
+        .to( target, 1000 )
+        .easing( TWEEN.Easing.Back.InOut )
         .onUpdate( render )
         .start();
 
