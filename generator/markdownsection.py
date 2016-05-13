dbd1e64c974b1837f6f2ade0f148f0e04b9c542f
Jack Morgan <jackmorgannz@gmail.com>
2016-05-13 17:29:28 +1200

    Altered Markdown parsing library and process (fixes #324 #335)
    Changes:
    - The Markdown library has been switched to
      Mistune which performs faster than Markdown2
      and also allows lists of greater than 2
      levels, fixing issue 324.
    - If a page is sectioned, then Markdown is
      only parsed between these tags.
    - If a page is not sectioned, the whole page
      is sent to the Markdown parser.
    - Inpage interactives have all preceeding
      whitespace removed to avoid confusion with
      the Markdown parser (as indentation is
      used in the Markdown syntax.
    - The generation of inline math has been
      simplified again, as the fix to avoid a bug
      in Markdown2 is not needed.
    
    Next steps:
    - Syntax highlighting is not provided in
      Mistune, however a community library adds
      this in, so this will need to be included.
    - Check for any existing cuddled-lists that
      are not processed in Mistune.
    - Check for content in sectioned chapters
      that missed being sent through the Markdown
      parser.

--- generator/markdownsection.py
+++ generator/markdownsection.py
@@ -7,7 +7,7 @@ import generator.systemfunctions as systemfunctions
 from bs4 import BeautifulSoup, Comment
 from generator.systemconstants import *
 from collections import OrderedDict
-from markdown2 import markdown
+import mistune
 from generator.files import setup_required_files
 
 MARKDOWN2_EXTRAS = ["code-friendly",
@@ -150,7 +150,7 @@ class Section:
                 summary = ': ' + summary_value.strip() if summary_value else ''
                 expanded_value = parse_argument('expanded', arguments)
                 expanded = ' active' if expanded_value == 'True' else ''
-                content = markdown(match.group('content'), extras=MARKDOWN2_EXTRAS)
+                content = self.parse_markdown(match.group('content'))
 
                 heading = self.html_templates['panel_heading'].format(title=title,
                                                                       summary=summary)
@@ -177,7 +177,7 @@ class Section:
         if content:
             indented_value = parse_argument('indented', arguments)
             indented = ' text-box-indented' if indented_value == 'True' else ''
-            content = markdown(content, extras=MARKDOWN2_EXTRAS).strip()
+            content = self.parse_markdown(content).strip()
             html = self.html_templates['text-box'].format(content=content, indented=indented)
         else:
             self.regex_functions['text-box'].log("Text box has zero content", self, match.group(0))
@@ -199,7 +199,6 @@ class Section:
         equation = match.group('equation')
         if match.group('type') == 'math':
             #Inline math
-            equation = re.sub("\\\\{1,}", self.double_backslashes, equation)
             html = self.html_templates['math'].format(equation=equation)
         else:
             #Block math
@@ -456,7 +455,7 @@ class Section:
 
     def process_code_block(self, match):
         """Create a button for linking to a page"""
-        html = markdown(match.group('code'), extras=["fenced-code-blocks"])
+        html = self.parse_markdown(match.group('code'))
         # This extra div block stops Markdown2 from not reading code blocks correctly
         return '<div>' + html + '</div>\n'
 
@@ -546,6 +545,8 @@ class Section:
             self.edit_interactive_tree(interactive_tree, source_folder)
             # Remove comments from HTML
             html = re.sub('(\n)*<!--(.|\s)*?-->(\n)*', '', str(interactive_tree).strip(), flags=re.MULTILINE)
+            # Remove preceeding whitespace on lines to avoid errors with Markdown parser
+            html = re.sub('^\s*', '', html, flags=re.MULTILINE)
             # Remove multiple consecutive newline characters from removal of <link> and <script> tags
             html = re.sub('\n{2,}', '\n', html, flags=re.MULTILINE)
             return html
@@ -655,7 +656,7 @@ class Section:
         arguments = match.group('args')
         term = parse_argument('term', arguments)
         definition = parse_argument('definition', arguments)
-        definition = markdown(definition, extras=MARKDOWN2_EXTRAS)
+        definition = self.parse_markdown(definition)
 
         permalink = self.create_permalink('glossary-' + term)
 
@@ -711,6 +712,29 @@ class Section:
 
     # ----- Parsing Functions -----
 
+
+    def parse_markdown(self, text):
+        return mistune.markdown(text,
+                                escape=False,
+                                hard_wrap=False,
+                                parse_block_html=False,
+                                parse_inline_html=False,
+                                use_xhtml=False)
+
+
+    def parse_section(self, match):
+        return match.group(1) + self.parse_markdown(match.group(2))
+
+
+    def parse_sections(self, text):
+        """Parses HTML within <sections>"""
+        html = re.sub('(\<section[^\>]*\>)([\s\S]*?)(?=\<\/section\>)',
+                      self.parse_section,
+                      text,
+                      flags=re.MULTILINE)
+        return html
+
+
     def parse_markdown_content(self, html_templates):
         """Converts raw Markdown into HTML.
 
@@ -726,18 +750,13 @@ class Section:
         for regex in self.regex_functions.values():
             text = re.sub(regex.expression, regex.function, text, flags=re.MULTILINE)
 
-        # Close last section if needed
+        # Close last section if needed and parse left over text with parser
         if self.sectioned:
             text += self.html_templates['section-end']
-
-        # Parse with library parser
-        text = markdown(text, extras=MARKDOWN2_EXTRAS)
-
-        # Remove any empty <p></p> tags
-        # TODO: Find why this occurs and stop it
-        text = re.sub("<p><\/p>", '', text, flags=re.MULTILINE)
-
-        self.html_content.append(text)
+            html = self.parse_sections(text)
+        else:
+            html = self.parse_markdown(text)
+        self.html_content.append(html)
 
         # Log error messages
         for regex in self.regex_functions.values():
