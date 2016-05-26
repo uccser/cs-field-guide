1184eaff41575047c3222d8f59910b9e4ad22479
Jack Morgan <jackmorgannz@gmail.com>
2016-05-26 15:55:19 +1200

    Draw dots on cards for binary-cards interactive

--- interactives/binary-cards/js/binary-cards.js
+++ interactives/binary-cards/js/binary-cards.js
@@ -12,10 +12,11 @@ $(document).ready(function () {
         updateDotCount();
     });
 
-    // Create cards within container
+    // Create cards within container and update count
     createCards(binaryValueSettings);
     updateDotCount();
 });
+
 
 // Sets up the cards for the interactive
 function createCards(binaryValueSettings) {
@@ -27,15 +28,95 @@ function createCards(binaryValueSettings) {
     }
 };
 
+
+// Returns the HTML for a card for a given value
 function createCard(value) {
     var cardContainer = $("<div class='binary-card-container'></div>");
     var card = $("<div class='binary-card'></div>");
     cardContainer.append(card);
-    card.append($("<div class='binary-card-side binary-card-front'>" + value + "</div>"))
-    card.append($("<div class='binary-card-side binary-card-back'></div>"))
+    var front = $("<div class='binary-card-side binary-card-front'></div>");
+    front.append(createDots(value));
+    front.append($("<div class='binary-card-number'>" + createCardLabel(value) + "</div>"));
+    card.append(front);
+    card.append($("<div class='binary-card-side binary-card-back'></div>"));
     card.data("value", value);
     return cardContainer;
 };
+
+
+// Returns a canvas object with the given number of dots drawn on it
+function createDots(dots) {
+    var canvas = document.createElement('canvas');
+    canvas.width = 120;
+    canvas.height = 120;
+    var context = canvas.getContext('2d');
+    context.imageSmoothingEnabled = true;
+
+    var sizes = calculateDotGridSize(dots);
+    var gridRows = sizes[0] + 1;
+    var gridRowWidth = canvas.height / gridRows;
+    var gridCols = sizes[1] + 1;
+    var gridColWidth = canvas.width / gridCols;
+    var dotSize = Math.min(gridRowWidth, gridColWidth) * 0.4;
+    var dotStartAngle = Math.PI * 0.5;
+    var dotFinishAngle = Math.min(Math.PI * 2, Math.PI * 2 * dots) + dotStartAngle;
+    for (var row = 1; row < gridRows; row++) {
+        for (var col = 1; col < gridCols; col++) {
+            context.beginPath();
+            context.arc(col * gridColWidth, row * gridRowWidth, dotSize, dotStartAngle, dotFinishAngle, false);
+            if (dots < 1) {
+                context.lineTo(col * gridColWidth, row * gridRowWidth);
+            }
+            context.closePath();
+            context.fill();
+        }
+    }
+    return canvas;
+};
+
+
+// Returns the HTML for the card label to represent a given
+// value. Decimals are represented as fractions.
+function createCardLabel(value) {
+    var label;
+    if (value < 1) {
+        label = '<sup>1</sup>&frasl;<sub>' + 1 / value + '</sub>';
+    } else {
+        label = value
+    }
+    return label
+}
+
+
+// Returns an array containing the [rows, columns]
+// to optimally display the grid of dots for the given
+// given number of dots
+function calculateDotGridSize(dots) {
+    var sizes;
+    if (dots >= 2) {
+        var factors = [];
+        for (var i = 1; i <= Math.floor(Math.sqrt(dots)); i += 1) {
+            if (dots % i === 0) {
+                factors.push(i);
+                if (dots / i !== i) {
+                    factors.push(dots / i);
+                }
+            }
+        }
+        factors.sort(function(a, b){return a - b;});  // numeric sort
+        var middleArrayIndex = factors.length / 2;
+
+        if (middleArrayIndex % 1) {
+            sizes = new Array(factors[middleArrayIndex - 0.5], factors[middleArrayIndex - 0.5]);
+        } else {
+            sizes = new Array(factors[middleArrayIndex], factors[middleArrayIndex - 1]);
+        }
+    } else {
+        sizes = [1, 1];
+    }
+    return sizes;
+};
+
 
 // Counts the number of dots on the cards
 function updateDotCount() {
