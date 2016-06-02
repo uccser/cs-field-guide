ec1ec30b47c763dd72d9344e1ed911e1baf49c6d
Jack Morgan <jackmorgannz@gmail.com>
2016-06-02 12:45:57 +1200

    Cache math image requests to avoid duplicate downloads

--- generator/print_media.py
+++ generator/print_media.py
@@ -7,6 +7,7 @@ class PrintRenderer(object):
 
     def __init__(self, generator_settings):
         self.generator_settings = generator_settings
+        self.math_images = {}
         self.math_image_number = 1
         self.math_temporary_folder = self.generator_settings['PDF']['Math Temporary Folder'].strip()
         self.math_temporary_filename = 'math-image-{num}.png'
@@ -14,16 +15,20 @@ class PrintRenderer(object):
 
     def render_math(self, formula):
         """Renders the given formula as an image"""
-        filename = self.math_temporary_filename.format(num=self.math_image_number)
-        file_location = os.path.join(self.math_temporary_folder, filename)
-        self.math_image_number += 1
+        if not formula in self.math_images:
+            filename = self.math_temporary_filename.format(num=self.math_image_number)
+            file_location = os.path.join(self.math_temporary_folder, filename)
+            self.math_image_number += 1
 
-        formula = formula.replace('\n', ' ')
-        request = requests.get( 'http://latex.codecogs.com/png.latex?\dpi{{300}} {formula}'.format(formula=formula))
-        image = open(file_location, 'wb')
-        image.write(request.content)
-        image.close()
-        print('Create {} from {}'.format(filename, formula))
+            formula = formula.replace('\n', ' ')
+            request = requests.get( 'http://latex.codecogs.com/png.latex?\dpi{{300}} {formula}'.format(formula=formula))
+            image = open(file_location, 'wb')
+            image.write(request.content)
+            image.close()
+            self.math_images[formula] = file_location
+            print('Create {} from {}'.format(filename, formula))
+        else:
+            file_location = self.math_images[formula]
         return file_location
 
     def create_temp_folder(self):
