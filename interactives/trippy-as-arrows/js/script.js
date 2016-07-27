e6783b39e3e9638a5e59f18f3bde9f7532c4bba1
hayleyavw <hayleyavw@gmail.com>
2016-07-28 11:29:42 +1200

    Testing out use of canvas element to generate a grid

--- interactives/trippy-as-arrows/js/script.js
+++ interactives/trippy-as-arrows/js/script.js
@@ -1,40 +1,28 @@
-/* WIP to form base templated for arrow manipulation interactives
- * Hayley van Waas
- */
+var ctx = document.getElementById("canvas").getContext("2d");
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-
-function onLoad() {
-    var graph = Viva.Graph.graph(),
-        nodePositions = [{x : -80, y: 0},
-        {x : 0, y: -80},
-        {x : 80, y: 0}, {x : -25, y: 0}, {x : 25, y: 0},
-        {x : -25, y : 150}, {x : 25, y : 150}], // predefined node positions
-        layout = Viva.Graph.Layout.constant(graph),
-        renderer = Viva.Graph.View.renderer(graph, {
-                       layout     : layout, // use our custom 'constant' layout
-                   }),
-        i, nodesCount = nodePositions.length; // convinience variables.
-    // Add nodes
-    for(i = 0; i < nodesCount; ++i) {
-        graph.addNode(i, nodePositions[i]);
-    }
-    // and make them connected
-
-    graph.addLink(0,1);
-    graph.addLink(1,2);
-    graph.addLink(2,4);
-    graph.addLink(4,6);
-    graph.addLink(6,5);
-    graph.addLink(5,3);
-    graph.addLink(3,0);
-
-    // set custom node placement callback for layout.
-    // if you don't do this, constant layout performs random positioning.
-    layout.placeNode(function(node) {
-        // node.id - points to its position but you can do your
-        // random logic here. E.g. read from specific node.data
-        // attributes. This callback is expected to return object {x : .. , y : .. }
-        return nodePositions[node.id];
-    });
-    renderer.run();
+window.onresize = function(event) {
+    // figure out a way to redraw canvas
+    // just calling draw function is too slow
+    // some optimisation required
 }
+
+function draw() {
+    console.log(window.innerWidth);
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+
+    for (var x = 0; x <= width; x+= 20) {
+        for (y = 0; y <= height; y += 20) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+            ctx.stroke();
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y);
+            ctx.stroke();
+        }
+    }
+
+}
+
---
+++ interactives/trippy-as-arrows/js/testig-viva-script.js
@@ -0,0 +1,40 @@
+/* WIP to form base templated for arrow manipulation interactives
+ * Hayley van Waas
+ */
+
+
+function onLoad() {
+    var graph = Viva.Graph.graph(),
+        nodePositions = [{x : -80, y: 0},
+        {x : 0, y: -80},
+        {x : 80, y: 0}, {x : -25, y: 0}, {x : 25, y: 0},
+        {x : -25, y : 150}, {x : 25, y : 150}], // predefined node positions
+        layout = Viva.Graph.Layout.constant(graph),
+        renderer = Viva.Graph.View.renderer(graph, {
+                       layout     : layout, // use our custom 'constant' layout
+                   }),
+        i, nodesCount = nodePositions.length; // convinience variables.
+    // Add nodes
+    for(i = 0; i < nodesCount; ++i) {
+        graph.addNode(i, nodePositions[i]);
+    }
+    // and make them connected
+
+    graph.addLink(0,1);
+    graph.addLink(1,2);
+    graph.addLink(2,4);
+    graph.addLink(4,6);
+    graph.addLink(6,5);
+    graph.addLink(5,3);
+    graph.addLink(3,0);
+
+    // set custom node placement callback for layout.
+    // if you don't do this, constant layout performs random positioning.
+    layout.placeNode(function(node) {
+        // node.id - points to its position but you can do your
+        // random logic here. E.g. read from specific node.data
+        // attributes. This callback is expected to return object {x : .. , y : .. }
+        return nodePositions[node.id];
+    });
+    renderer.run();
+}
