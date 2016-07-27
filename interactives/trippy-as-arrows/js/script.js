61da3c2cb77a98c7a1d9c5ca0a9a0af1435869b2
hayleyavw <hayleyavw@gmail.com>
2016-07-27 13:44:42 +1200

    Put arrow back on page

--- interactives/trippy-as-arrows/js/script.js
+++ interactives/trippy-as-arrows/js/script.js
@@ -1,6 +1,13 @@
+/* WIP to form base templated for arrow manipulation interactives
+ * Hayley van Waas
+ */
+
+
 function onLoad() {
     var graph = Viva.Graph.graph(),
-        nodePositions = [{x : -80, y: 0}, {x : 0, y: -80}, {x : 80, y: 0}, {x : -25, y: 0}, {x : 25, y: 0},
+        nodePositions = [{x : -80, y: 0},
+        {x : 0, y: -80},
+        {x : 80, y: 0}, {x : -25, y: 0}, {x : 25, y: 0},
         {x : -25, y : 150}, {x : 25, y : 150}], // predefined node positions
         layout = Viva.Graph.Layout.constant(graph),
         renderer = Viva.Graph.View.renderer(graph, {
@@ -11,7 +18,7 @@ function onLoad() {
     for(i = 0; i < nodesCount; ++i) {
         graph.addNode(i, nodePositions[i]);
     }
-    // and make them connected 
+    // and make them connected
 
     graph.addLink(0,1);
     graph.addLink(1,2);
@@ -20,7 +27,7 @@ function onLoad() {
     graph.addLink(6,5);
     graph.addLink(5,3);
     graph.addLink(3,0);
-    
+
     // set custom node placement callback for layout.
     // if you don't do this, constant layout performs random positioning.
     layout.placeNode(function(node) {
