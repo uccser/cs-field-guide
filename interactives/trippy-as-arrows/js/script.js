c48ef90efc87138390330e153c1b318637ddae71
hayleyavw <hayleyavw@gmail.com>
2016-07-28 13:32:08 +1200

    Positioned canvas and instruction div on page
    Dynamically changes the size of the grid on page resize

--- interactives/trippy-as-arrows/js/script.js
+++ interactives/trippy-as-arrows/js/script.js
@@ -1,20 +1,30 @@
 var ctx = document.getElementById("canvas").getContext("2d");
-var width = window.innerWidth;
-var height = window.innerHeight;
+
+var square_size = 40;
+
+// set width and height
+// take mod square_size to match dimensions of each square in the grid (removes trailing lines)
+// set width of canvas to be 75% of the width of the page to leave room for instructions
+var base_width = window.innerWidth * 0.75;
+var width = base_width - (base_width % square_size);
+var base_height = window.innerHeight * 0.95;
+var height = base_height - (base_height % square_size);
 
 window.onresize = function(event) {
     // figure out a way to redraw canvas
     // just calling draw function is too slow
     // some optimisation required
+    window.requestAnimationFrame(draw);
 }
 
+
 function draw() {
-    console.log(window.innerWidth);
+
     ctx.canvas.width = width;
     ctx.canvas.height = height;
 
-    for (var x = 0; x <= width; x+= 20) {
-        for (y = 0; y <= height; y += 20) {
+    for (var x = 0; x <= width; x+= square_size) {
+        for (y = 0; y <= height; y += square_size) {
             ctx.moveTo(x, 0);
             ctx.lineTo(x, height);
             ctx.stroke();
