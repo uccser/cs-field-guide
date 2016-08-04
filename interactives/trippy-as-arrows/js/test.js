437c46b95e3df7defc53cb6c20a868e9d6b6db3f
Hayley van Waas <hayleyavw@gmail.com>
2016-08-04 22:06:57 +1200

    WIP still playing with vivagraph to try and make nodes static

--- interactives/trippy-as-arrows/js/test.js
+++ interactives/trippy-as-arrows/js/test.js
@@ -112,55 +112,53 @@ function drawArrow() {
 
 
 function test() {
-	var graphGenerator = Viva.Graph.generator();
-var graph = graphGenerator.grid(3, 3);
+     var graph = Viva.Graph.graph();
+         nodePositions = [
+             {x : -80, y: 0},
+             {x : 0, y: -80},
+             {x : 80, y: 0},
+             {x : -25, y: 0},
+             {x : 25, y: 0},
+             {x : -25, y : 150},
+             {x : 25, y : 150}]; // predefined node positions
+         layout = Viva.Graph.Layout.constant(graph);
 
-var layout = Viva.Graph.Layout.forceDirected(graph, {
-    springLength : 10,
-    springCoeff : 0.0005,
-    dragCoeff : 0.02,
-    gravity : -1.2
-});
+         renderer = Viva.Graph.View.renderer(graph, {
+                        layout     : layout, // use our custom 'constant' layout
+                    });
+         nodesCount = nodePositions.length; // convinience variables.
+     // Add nodes
+     for(i = 0; i < nodesCount; ++i) {
+         graph.addNode(i, nodePositions[i]);
+     }
+     // and make them connected
 
-var renderer = Viva.Graph.View.renderer(graph, {
-    layout : layout
-});
-renderer.run();
-    // var graph = Viva.Graph.graph(),
-    //     nodePositions = [
-    //         {x : -80, y: 0},
-    //         {x : 0, y: -80},
-    //         {x : 80, y: 0},
-    //         {x : -25, y: 0},
-    //         {x : 25, y: 0},
-    //         {x : -25, y : 150},
-    //         {x : 25, y : 150}], // predefined node positions
-    //     layout = Viva.Graph.Layout.constant(graph),
-    //     renderer = Viva.Graph.View.renderer(graph, {
-    //                    layout     : layout, // use our custom 'constant' layout
-    //                }),
-    //     i, nodesCount = nodePositions.length; // convinience variables.
-    // // Add nodes
-    // for(i = 0; i < nodesCount; ++i) {
-    //     graph.addNode(i, nodePositions[i]);
-    // }
-    // // and make them connected
+     graph.addLink(0,1);
+     graph.addLink(1,2);
+     graph.addLink(2,4);
+     graph.addLink(4,6);
+     graph.addLink(6,5);
+     graph.addLink(5,3);
+     graph.addLink(3,0);
 
-    // graph.addLink(0,1);
-    // graph.addLink(1,2);
-    // graph.addLink(2,4);
-    // graph.addLink(4,6);
-    // graph.addLink(6,5);
-    // graph.addLink(5,3);
-    // graph.addLink(3,0);
+     // set custom node placement callback for layout.
+     // if you don't do this, constant layout performs random positioning.
+     layout.placeNode(function(node) {
+         // node.id - points to its position but you can do your
+         // random logic here. E.g. read from specific node.data
+         // attributes. This callback is expected to return object {x : .. , y : .. }
+         return nodePositions[node.id];
+     });
 
-    // // set custom node placement callback for layout.
-    // // if you don't do this, constant layout performs random positioning.
-    // layout.placeNode(function(node) {
-    //     // node.id - points to its position but you can do your
-    //     // random logic here. E.g. read from specific node.data
-    //     // attributes. This callback is expected to return object {x : .. , y : .. }
-    //     return nodePositions[node.id];
-    // });
-    // renderer.run();
+     // TODO working here
+     var graphics = Viva.Graph.View.svgGraphics();
+     graphics.node(function(node) {
+         var ui = Viva.Graph.svg("rect");
+         uni.addEventListener('click', function() {
+             layout.pinNode(node, !layout.isNodePinned(node));
+         });
+         return ui;
+     });
+
+     renderer.run();
 }
