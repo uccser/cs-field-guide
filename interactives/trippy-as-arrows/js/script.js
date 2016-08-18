a08ac67834dcccba294d0721a91f88904c7c8980
hayleyavw <hayleyavw@gmail.com>
2016-08-18 17:09:02 +1200

    Now dynamically decides where to place x and y intercept
     - Means that they are now aligned with the rest of the grid
     - Wrote some really gross code that should go into a function....

--- interactives/trippy-as-arrows/js/script.js
+++ interactives/trippy-as-arrows/js/script.js
@@ -1,27 +1,41 @@
-//var ctx = document.getElementById("canvas").getContext("2d");
+//var ctx = document.getElementById('canvas').getContext('2d');
 
-//TODO convert to style guide -camel case not snake case
+// TODO set container width/height from within js
 
-var squareSize = 40;
-var baseWidth = window.innerWidth * 0.75;
-var width = baseWidth - (baseWidth % squareSize);
-var baseHeight = window.innerHeight * 0.95;
-var height = baseHeight - (baseHeight % squareSize);
+/* NTS this is gross code
+ * should go in a function or something
+ */
+var container = document.getElementById('container'); // NTS is it more efficient to do this, or to access it directly everytime?...
+var squareSize = 20;
 
-// TODO there must be a better way to do this - onload?
+var containerWidth = container.offsetWidth;
+
+var numSquares = Math.floor(containerWidth / squareSize);
+var midPoint = Math.floor(numSquares / 2);
+var xIntercept = midPoint * squareSize;
+
+var containerHeight = container.offsetHeight;
+numSquares = Math.floor(containerHeight / squareSize);
+midPoint = Math.floor(numSquares / 2);
+yIntercept = midPoint * squareSize;
+
+/* end gross code */
+
+// NTS there must be a better way to do this - onload?
 drawBackground();
 test();
 
+/*
+ * Draws the graph background
+ * TODO needs to update on window resize
+ */
 function drawBackground() {
-    //document.body.style.backgroundSize = "50% 50%, 50% 50%, 10px 10px, 10px 10px";
-    var squareSize = 20;
-    var baseWidth = window.innerWidth;
-    //var width = baseWidth - (baseWidth % squareSize);
-    var width = (baseWidth - (baseWidth % 20)) / 2;
-    console.log(width);
 
-    var backgroungSizeFormat = width + "px " + width + "px, " + width + "px " + width + "px, " + squareSize + "px " + squareSize + "px, " + squareSize + "px " + squareSize + "px";
-    document.body.style.backgroundSize = backgroungSizeFormat;
+    // Buid the string for creating the grid background
+    var backgroundSizeFormat = xIntercept + 'px ' + yIntercept + 'px, ' + xIntercept + 'px ' + yIntercept + 'px, ' + squareSize + 'px ' + squareSize + 'px, ' + squareSize + 'px ' + squareSize + 'px';
+
+    // Apply the background styling to the container element
+    container.style.backgroundSize = backgroundSizeFormat;
 }
 
 
@@ -46,49 +60,49 @@ function drawArrow() {
     // create new space to draw
     ctx.beginPath();
 
-    var x = width / 2;
+    var x = xIntercept / 2;
     // taking mod of height ensures that the arrow starts exactly on a line
-    var y = (height / 3) - ((height / 3) % squareSize);
+    var y = (yIntercept / 3) - ((yIntercept / 3) % squareSize);
 
     var arrowWidth = 2.5;
     var arrowHeight = 7;
 
     var coordinates = {
-        "p1": [x, y],
-        "p2": [x - (arrowWidth * squareSize), y + (arrowWidth * squareSize)],
-        "p3": [x - (squareSize), y + (arrowWidth * squareSize)],
-        "p4": [x - (squareSize), y + (arrowHeight * squareSize)],
-        "p5": [x + (squareSize), y + (arrowHeight * squareSize)],
-        "p6": [x + (squareSize), y + (arrowWidth * squareSize)],
-        "p7": [x + (arrowWidth * squareSize), y + (arrowWidth * squareSize)]
+        'p1': [x, y],
+        'p2': [x - (arrowWidth * squareSize), y + (arrowWidth * squareSize)],
+        'p3': [x - (squareSize), y + (arrowWidth * squareSize)],
+        'p4': [x - (squareSize), y + (arrowHeight * squareSize)],
+        'p5': [x + (squareSize), y + (arrowHeight * squareSize)],
+        'p6': [x + (squareSize), y + (arrowWidth * squareSize)],
+        'p7': [x + (arrowWidth * squareSize), y + (arrowWidth * squareSize)]
     }
 
-    ctx.moveTo(coordinates["p1"][0], coordinates["p1"][1]);
-    ctx.lineTo(coordinates["p2"][0], coordinates["p2"][1]);
-    ctx.lineTo(coordinates["p3"][0], coordinates["p3"][1]);
-    ctx.lineTo(coordinates["p4"][0], coordinates["p4"][1]);
-    ctx.lineTo(coordinates["p5"][0], coordinates["p5"][1]);
-    ctx.lineTo(coordinates["p6"][0], coordinates["p6"][1]);
-    ctx.lineTo(coordinates["p7"][0], coordinates["p7"][1]);
-    ctx.moveTo(coordinates["p1"][0], coordinates["p1"][1]);
+    ctx.moveTo(coordinates['p1'][0], coordinates['p1'][1]);
+    ctx.lineTo(coordinates['p2'][0], coordinates['p2'][1]);
+    ctx.lineTo(coordinates['p3'][0], coordinates['p3'][1]);
+    ctx.lineTo(coordinates['p4'][0], coordinates['p4'][1]);
+    ctx.lineTo(coordinates['p5'][0], coordinates['p5'][1]);
+    ctx.lineTo(coordinates['p6'][0], coordinates['p6'][1]);
+    ctx.lineTo(coordinates['p7'][0], coordinates['p7'][1]);
+    ctx.moveTo(coordinates['p1'][0], coordinates['p1'][1]);
 
     // Show value in input box
     // x values
-    document.getElementById('p1-input-x').value = coordinates["p1"][0];
-    document.getElementById('p2-input-x').value = coordinates["p2"][0];
-    document.getElementById('p3-input-x').value = coordinates["p3"][0];
-    document.getElementById('p4-input-x').value = coordinates["p4"][0];
-    document.getElementById('p5-input-x').value = coordinates["p5"][0];
-    document.getElementById('p6-input-x').value = coordinates["p6"][0];
-    document.getElementById('p7-input-x').value = coordinates["p7"][0];
+    document.getElementById('p1-input-x').value = coordinates['p1'][0];
+    document.getElementById('p2-input-x').value = coordinates['p2'][0];
+    document.getElementById('p3-input-x').value = coordinates['p3'][0];
+    document.getElementById('p4-input-x').value = coordinates['p4'][0];
+    document.getElementById('p5-input-x').value = coordinates['p5'][0];
+    document.getElementById('p6-input-x').value = coordinates['p6'][0];
+    document.getElementById('p7-input-x').value = coordinates['p7'][0];
     // y values
-    document.getElementById('p1-input-y').value = coordinates["p1"][1];
-    document.getElementById('p2-input-y').value = coordinates["p2"][1];
-    document.getElementById('p3-input-y').value = coordinates["p3"][1];
-    document.getElementById('p4-input-y').value = coordinates["p4"][1];
-    document.getElementById('p5-input-y').value = coordinates["p5"][1];
-    document.getElementById('p6-input-y').value = coordinates["p6"][1];
-    document.getElementById('p7-input-y').value = coordinates["p7"][1];
+    document.getElementById('p1-input-y').value = coordinates['p1'][1];
+    document.getElementById('p2-input-y').value = coordinates['p2'][1];
+    document.getElementById('p3-input-y').value = coordinates['p3'][1];
+    document.getElementById('p4-input-y').value = coordinates['p4'][1];
+    document.getElementById('p5-input-y').value = coordinates['p5'][1];
+    document.getElementById('p6-input-y').value = coordinates['p6'][1];
+    document.getElementById('p7-input-y').value = coordinates['p7'][1];
 
 
 }
@@ -136,7 +150,7 @@ function test() {
      // TODO working here
      var graphics = Viva.Graph.View.svgGraphics();
      graphics.node(function(node) {
-         var ui = Viva.Graph.svg("rect");
+         var ui = Viva.Graph.svg('rect');
          uni.addEventListener('click', function() {
              layout.pinNode(node, !layout.isNodePinned(node));
          });
