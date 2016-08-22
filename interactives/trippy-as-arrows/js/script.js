3d6fbda18d55f290718f96741cc459026f7b267c
hayleyavw <hayleyavw@gmail.com>
2016-08-22 13:28:49 +1200

    Broke long function up into 3, and created class for new points

--- interactives/trippy-as-arrows/js/script.js
+++ interactives/trippy-as-arrows/js/script.js
@@ -5,6 +5,11 @@
 // NTS maybe use additional svgs of small circles that sit on top of each point (only show when hovered)
 // NTS passing around dimensions variable - might as well be global?
 
+/* Point class for generating new points */
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
 
 window.onload = function() {
@@ -78,49 +83,70 @@ function drawArrow(dimensions) {
     var arrowHeight = 8;
     var offset = arrowHeight / 2;
 
-    var p0 = polygon.points.getItem(0);
+    var p0 = new Point();
     p0.x = dimensions.xIntercept;
     p0.y = dimensions.yIntercept - offset * dimensions.squareSize;
 
-    var p1 = polygon.points.getItem(1);
+    var p1 = new Point();
     p1.x = dimensions.xIntercept - (arrowWidth * dimensions.squareSize);
     p1.y = dimensions.yIntercept + (arrowWidth * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    var p2 = polygon.points.getItem(2);
+    var p2 = new Point();
     p2.x = dimensions.xIntercept - dimensions.squareSize;
     p2.y = dimensions.yIntercept + (arrowWidth * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    var p3 = polygon.points.getItem(3);
+    var p3 = new Point();
     p3.x = dimensions.xIntercept - dimensions.squareSize;
     p3.y = dimensions.yIntercept + (arrowHeight * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    var p4 = polygon.points.getItem(4);
+    var p4 = new Point();
     p4.x = dimensions.xIntercept + dimensions.squareSize;
     p4.y = dimensions.yIntercept + (arrowHeight * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    var p5 = polygon.points.getItem(5);
+    var p5 = new Point();
     p5.x = dimensions.xIntercept + dimensions.squareSize;
     p5.y = dimensions.yIntercept + (arrowWidth * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    var p6 = polygon.points.getItem(6);
+    var p6 = new Point();
     p6.x = dimensions.xIntercept + (arrowWidth * dimensions.squareSize);
     p6.y = dimensions.yIntercept + (arrowWidth * dimensions.squareSize) - offset * dimensions.squareSize;
 
-    updateInputBoxes([p0, p1, p2, p3, p4, p5, p6]); // NTS maybe this list should be a variable
+    var newPoints = [p0, p1, p2, p3, p4, p5, p6];
+
+    updateArrow(newPoints);
+    updateInputBoxes(newPoints, dimensions); // NTS maybe this list should be a variable
 
 }
 
 
-function updateInputBoxes(points) {
+/* Updates each coordinate in the arrow */
+// TODO for input boxes, might just be able to link each input to corresponding point on the arrow
+function updateArrow(newPoints) {
+
+    var polygon = document.getElementsByTagName('polygon')[0]; // the svg arrow NTS or should this be a parameter?
+    var point;
+    for (var i = 0; i < newPoints.length; i++) {
+        point = polygon.points.getItem(i);
+        point.x = newPoints[i].x;
+        point.y = newPoints[i].y;
+    }
+
+}
+
+
+/* Put the corresponding coordinate into each of the input boxes
+ * Offsets the real value of the coordinate to give impression that centre of grid is position (0,0)
+ * */
+function updateInputBoxes(points, dimensions) {
 
     var inputId = '';
 
     // uses index to determine which input box to reference
     for (var i = 0; i < points.length; i++) {
         inputId = 'p' + i + '-input-x';
-        document.getElementById(inputId).value = points[i].x;
+        document.getElementById(inputId).value = points[i].x - dimensions.xIntercept;
         inputId = 'p' + i + '-input-y';
-        document.getElementById(inputId).value = points[i].y;
+        document.getElementById(inputId).value = points[i].y - dimensions.yIntercept;
     }
 
 }
