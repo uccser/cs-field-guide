18771a4f7849e6d5963565cad23fbde8cf953368
Jack Morgan <jackmorgannz@gmail.com>
2016-10-12 22:44:40 +1300

    Alter high-score-boxes image URLs to new image locations

--- interactives/high-score-boxes/js/find-high-score.js
+++ interactives/high-score-boxes/js/find-high-score.js
@@ -110,7 +110,7 @@ function createBoxElements() {
 		box_image.id = 'box' + i;
 		box_image.className = 'box';
         box_image.draggable = false;
-		box_image.src = './img/square' + (box_images[i] + 1) + '.png';
+		box_image.src = '../base-files/img/colourful-box-images/square' + (box_images[i] + 1) + '-200px-rounded.png';
 		currentboxObject.divElement = box_image;
 
 		//divs that hold the numbers
