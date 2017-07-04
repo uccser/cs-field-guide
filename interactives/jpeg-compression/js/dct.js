2300527a2df14314fd4b45fdd8b2f8aa9a54da2f
Isabelle Taylor <isabelle.t732@gmail.com>
2017-07-05 11:16:56 +1200

    Added a grid of numbers over the 8x8 square with a toggle to switch it off

--- interactives/jpeg-compression/js/dct.js
+++ interactives/jpeg-compression/js/dct.js
@@ -6,10 +6,14 @@ $(function() {
 
     var big_canvas = document.getElementById("before_image_canvas");
     var big_canvas_context = big_canvas.getContext("2d");
-    var small_canvas = document.getElementById("8_by_8_before");
+    var small_canvas = document.getElementById("before_8_by_8");
     var small_canvas_ctx = small_canvas.getContext("2d");
     var placeholder_canvas = document.getElementById("placeholder_canvas");
     var placeholder_canvas_context = placeholder_canvas.getContext("2d");
+    var quantisation = [16, 11, 12, 15, 21, 32, 50, 66, 11, 12, 13, 18, 24, 46, 62, 73, 12, 13,
+        16, 23, 38, 56, 73, 75, 15, 18, 23, 29, 53, 75, 83, 83, 21, 24, 38, 53, 68, 95, 103, 103, 32, 46,
+        56, 75, 95, 104, 117, 117, 50, 62, 73, 83, 103, 117, 120, 120, 66, 73, 75, 83, 103, 117, 120, 120
+    ];
     //No smoothing please
     small_canvas_ctx.mozImageSmoothingEnabled = false;
     small_canvas_ctx.webkitImageSmoothingEnabled = false;
@@ -23,11 +27,15 @@ $(function() {
     big_canvas_context.webkitImageSmoothingEnabled = false;
     big_canvas_context.msImageSmoothingEnabled = false;
     big_canvas_context.ImageSmoothingEnabled = false;
-    small_canvas_ctx.scale(20, 20);
+    small_canvas_ctx.scale(30, 30);
 
     create_dct_table();
+    createLabelGrid();
     drag_8_by_8();
     draw_big_image();
+    $("#toggleNumber").change(function() {
+        $("#labels_8_by_8").toggle("fade");
+    });
 
 
     /* Creates html table that contains the DCT images and the (changeable) value 
@@ -38,7 +46,7 @@ $(function() {
             var row = $('<tr></tr>');
             for (var j = 1; j <= 8; j++) {
                 var rowData = $('<td></td>').append('<img src="./img/basis_functions/DCTr' + i + 'c' + j + '.png">');
-                rowData.append('<div class="stepper" id="stepper_' + i + j + '"><div class="stepper-progress"></div><input type="text" class="stepper-number"></div>');
+                rowData.append('<div class="stepper" id="stepper_' + i + j + '"><div class="stepper-progress"></div><input type="text" class="stepper-number" id="stepper_number_' + i + j + '"></div>');
                 row.append(rowData);
             };
             $('#dct_table').append(row);
@@ -46,16 +54,29 @@ $(function() {
 
         for (var i = 1; i <= 8; i++) {
             for (var j = 1; j <= 8; j++) {
-                $('#stepper_' + i + j).stepper({
-                    unit: '',
-                    initialValue: 0,
-                    min: -1024,
-                    max: 1023,
-                    stepSize: 1
-                });
+                if (i != 1 || j != 1) {
+                    $('#stepper_' + i + j).stepper({
+                        unit: '',
+                        initialValue: 0,
+                        min: -1024,
+                        max: 1023,
+                        stepSize: 1
+                    });
+                }
             };
         };
+        $('#stepper_11').stepper({
+            unit: '',
+            initialValue: 0,
+            min: 0,
+            max: 2048,
+            stepSize: 1
+        });
+
+
     }
+
+
 
     function drag_8_by_8() {
         // target elements with the "draggable" class
@@ -85,21 +106,33 @@ $(function() {
             });
 
         function dragMoveListener(event) {
-            var target = event.target,
-                // keep the dragged position in the data-x/data-y attributes
-                x = Math.max(Math.floor((parseFloat(target.getAttribute('data-x')) || 0) + event.dx), 0),
-                y = Math.max(Math.floor((parseFloat(target.getAttribute('data-y')) || 0) + event.dy), 0);
+            var target = event.target;
+            // keep the dragged position in the data-x/data-y attributes
+
+            var x = Math.max(Math.floor((parseFloat(target.getAttribute('data-x')) || 0) + event.dx), 0);
+            var y = Math.max(Math.floor((parseFloat(target.getAttribute('data-y')) || 0) + event.dy), 0);
 
             // translate the element
             target.style.webkitTransform =
                 target.style.transform =
                 'translate(' + x + 'px, ' + y + 'px)';
 
-            // update the posiion attributes
+            // update the position attributes
             target.setAttribute('data-x', x);
             target.setAttribute('data-y', y);
             close_up_8_by_8(x, y);
         }
+    }
+
+    function createLabelGrid() {
+        for (var i = 1; i <= 8; i++) {
+            var row = $('<tr class="label_row"></tr>');
+            for (var j = 1; j <= 8; j++) {
+                var rowData = $('<td class="label_data" id="cell' + i + j + '"></td>').append('0');
+                row.append(rowData);
+            };
+            $('#labels_8_by_8').append(row);
+        };
     }
 
     function close_up_8_by_8(x, y) {
@@ -110,6 +143,7 @@ $(function() {
             yData.push(pixels[i]);
         }
         updateDCTValues(dct(yData));
+        updateLabelGrid(yData);
         placeholder_canvas_context.putImageData(imageData, 0, 0);
 
         var zoomed_bit = new Image();
@@ -122,11 +156,27 @@ $(function() {
 
     }
 
+    function updateLabelGrid(data) {
+        index = 0;
+        for (var i = 1; i <= 8; i++) {
+            for (var j = 1; j <= 8; j++) {
+                $('#cell' + i + j).text(data[index])
+                if (data[index] > 127) {
+                    $('#cell' + i + j).css('color', 'black');
+                } else {
+                    $('#cell' + i + j).css('color', 'white');
+                }
+                index++
+            };
+        }
+    }
+
+
     function updateDCTValues(data) {
         var index = 0;
         for (var i = 1; i <= 8; i++) {
             for (var j = 1; j <= 8; j++) {
-                $('#stepper_' + i + j).text(Math.round((data[index] + 0.00001) * 100) / 100);
+                $('#stepper_number_' + i + j).val(Math.round((data[index] + 0.00001) * 100) / 100);
                 index++;
             };
         };
@@ -136,8 +186,7 @@ $(function() {
         var big_canvas = document.getElementById("before_image_canvas");
         var big_canvas_context = big_canvas.getContext("2d");
         var img = new Image();
-        img.src = "./img/gerbera.jpg";
-        img.crossOrigin = "Anonymous";
+        img.src = "./img/dandelion.jpg";
         img.onload = function() {
             big_canvas_context.drawImage(img, 0, 0, 320, 320);
             var imageData = big_canvas_context.getImageData(0, 0, 320, 320);
@@ -152,6 +201,18 @@ $(function() {
             close_up_8_by_8(0, 0);
         };
     }
+
+    // $(big_canvas).click(function(jqEvent) {
+    // var coords = {
+    //     x: jqEvent.pageX - $(big_canvas).offset().left,
+    //     y: jqEvent.pageY - $(big_canvas).offset().top};
+    //     //$("#little_drag_square").css("transform":"translate(" + x + "px," + y + "px)");
+    // var event = new CustomEvent("clickCanvas");
+    // event.target = $("#little_drag_square").get();
+    // event.target.setAttribute("data-x", coords.x);
+    // event.target.setAttribute("data-y", coords.y);
+    // dragMoveListener(event);
+    // });
 
     var dct = function(input) {
         var output = [],
