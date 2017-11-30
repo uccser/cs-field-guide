818c90a4f9dd7eabbd2e42f330a96fd1d818a841
Hayley van Waas <hayleyavw@gmail.com>
2017-11-30 16:42:33 +1300

    Added test for chapter models, urls and views

---
+++ csfieldguide/tests/chapters/models/test_chapter_model.py
@@ -0,0 +1,41 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from chapters.models import Chapter
+
+
+class ChapterModelTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test_one_chapter(self):
+        """
+        """
+        new_chapter_term = Chapter.objects.create(
+            slug="chapter-1",
+            name="Chapter 1",
+            content="This is the content for chapter 1."
+        )
+        query_result = Chapter.objects.get(slug="chapter-1")
+        self.assertEqual(query_result, new_chapter_term)
+
+    def test_two_chapters(self):
+        """
+        """
+        Chapter.objects.create(
+            slug="chapter-1",
+            name="Chapter 1",
+            content="This is the content for chapter 1."
+        )
+        Chapter.objects.create(
+            slug="chapter-2",
+            name="Chapter 2",
+            content="This is the content for chapter 2."
+        )
+        self.assertQuerysetEqual(
+            Chapter.objects.all(),
+            [
+                "<Chapter: Chapter 1>",
+                "<Chapter: Chapter 2>"
+            ],
+            ordered=False
+        )
---
+++ csfieldguide/tests/chapters/views/test_chapter_view.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+from tests.BaseTestWithDB import BaseTestWithDB
+from tests.chapters.ChaptersTestDataGenerator import ChaptersTestDataGenerator
+from django.urls import reverse
+
+
+class ChapterViewTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_data = ChaptersTestDataGenerator()
+        self.language = "en"
+
+    def test_chapter_view_with_valid_slugs(self):
+        """
+        """
+        self.test_data.create_chapter("1")
+
+        kwargs = {
+            "chapter_slug": "chapter-1"
+        }
+
+        url = reverse("chapters:chapter", kwargs=kwargs)
+        self.assertEqual(url, "/en/chapters/chapter-1/")
+
+        response = self.client.get(url)
+        self.assertEqual(HTTPStatus.OK, response.status_code)
