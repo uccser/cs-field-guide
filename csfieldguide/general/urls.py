907995471d143cfeba4b1834c6b527565560f4b6
Hayley van Waas <hayleyavw@gmail.com>
2017-12-03 11:21:25 +1300

    Added releases page

--- csfieldguide/general/urls.py
+++ csfieldguide/general/urls.py
@@ -5,22 +5,28 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# e.g. csfieldguide.com/
+    # e.g. csfieldguide.com/
     url(
-    	r"^$",
-    	views.GeneralIndexView.as_view(),
-    	name="home"
+        r"^$",
+        views.GeneralIndexView.as_view(),
+        name="home"
     ),
     # e.g. /about
     url(
-    	r"^about$",
-    	views.GeneralAboutView.as_view(),
-    	name="about"
+        r"^about$",
+        views.GeneralAboutView.as_view(),
+        name="about"
     ),
     # e.g. /contributors
     url(
-    	r"^contributors$",
-    	views.GeneralContributorsView.as_view(),
-    	name="contributors"
+        r"^contributors$",
+        views.GeneralContributorsView.as_view(),
+        name="contributors"
+    ),
+    # e.g. /releases
+    url(
+        r"^releases$",
+        views.GeneralReleasesView.as_view(),
+        name="releases"
     )
 ]
