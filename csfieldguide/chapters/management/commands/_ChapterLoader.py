15c13207a0b63e869a53ae9c5d0b82cfaaa5654a
Hayley van Waas <hayleyavw@gmail.com>
2018-01-29 17:29:52 +1300

    Added more error handling to chapter loader

--- csfieldguide/chapters/management/commands/_ChapterLoader.py
+++ csfieldguide/chapters/management/commands/_ChapterLoader.py
@@ -1,9 +1,14 @@
 """Custom loader for loading a topic."""
 
 import os.path
+
 from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
+
 from utils.BaseLoader import BaseLoader
 from utils.errors.MissingRequiredFieldError import MissingRequiredFieldError
+from utils.errors.KeyNotFoundError import KeyNotFoundError
+
 from chapters.models import Chapter
 from interactives.models import Interactive
 
@@ -25,13 +30,15 @@ class ChapterLoader(BaseLoader):
         self.chapter_slug = chapter_slug
         self.chapter_structure = chapter_structure
         self.BASE_PATH = os.path.join(BASE_PATH, "chapters")
+        self.chapter_required_fields = ["number", "icon"]
 
     @transaction.atomic
     def load(self):
         """Load the content for a chapter.
 
         Raise:
-            MissingRequiredFieldError: when no object can be found with the matching attribute.
+            MissingRequiredFieldError: when a config file does not contain a required field.
+            KeyNotFoundError: when no object can be found with the matching attribute.
         """
         # Convert the content to HTML
         chapter_content = self.convert_md_file(
@@ -46,11 +53,17 @@ class ChapterLoader(BaseLoader):
         if chapter_number is None:
             raise MissingRequiredFieldError(
                 self.structure_file_path,
-                ["number"],
-                "Chapter Number"
+                self.chapter_required_fields,
+                "Chapter"
             )
 
         chapter_icon = self.chapter_structure.get('icon', None)
+        if chapter_icon is None:
+            raise MissingRequiredFieldError(
+                self.structure_file_path,
+                self.chapter_required_fields,
+                "Chapter"
+            )
 
         # Create chapter object and save to the db
         chapter = Chapter(
@@ -64,11 +77,17 @@ class ChapterLoader(BaseLoader):
         chapter.save()
 
         chapter_interactives = self.chapter_structure.get('interactives', None)
-        # TODO error handling
         if chapter_interactives:
             for interactive_slug in chapter_interactives:
-                interactive = Interactive.objects.get(slug=interactive_slug)
-                chapter.interactives.add(interactive)
+                try:
+                    interactive = Interactive.objects.get(slug=interactive_slug)
+                    chapter.interactives.add(interactive)
+                except ObjectDoesNotExist:
+                    raise KeyNotFoundError(
+                        self.structure_file_path,
+                        interactive_slug,
+                        "Interactive"
+                    )
 
         self.log("Added Chapter: {}".format(chapter.name))
 
--- csfieldguide/tests/chapters/loaders/test_chapters_loader.py
+++ csfieldguide/tests/chapters/loaders/test_chapters_loader.py
@@ -1,7 +1,10 @@
 from tests.BaseTestWithDB import BaseTestWithDB
 from tests.chapters.ChaptersTestDataGenerator import ChaptersTestDataGenerator
+
 from chapters.management.commands._ChapterLoader import ChapterLoader
 from chapters.models import Chapter
+
+from utils.errors.KeyNotFoundError import KeyNotFoundError
 from utils.errors.MissingRequiredFieldError import MissingRequiredFieldError
 from utils.errors.EmptyMarkdownFileError import EmptyMarkdownFileError
 from utils.errors.NoHeadingFoundInMarkdownFileError import NoHeadingFoundInMarkdownFileError
@@ -19,7 +22,7 @@ class ChapterLoaderTest(BaseTestWithDB):
     def test_chapters_chapter_loader_single_chapter(self):
         config_file = "basic-config.yaml"  # placeholder, required parameter for error raised in chapter loader
         chapter_slug = "chapter-1"
-        chapter_structure = {"number": 1}
+        chapter_structure = {"number": 1, "icon": "legitimage.png"}
 
         chapter_loader = ChapterLoader(
             structure_file_path=config_file,
@@ -37,7 +40,7 @@ class ChapterLoaderTest(BaseTestWithDB):
     def test_chapters_chapter_loader_missing_chapter_number(self):
         config_file = "basic-config.yaml"
         chapter_slug = "chapter-1"
-        chapter_structure = {}
+        chapter_structure = {"icon": "image.png"}
 
         chapter_loader = ChapterLoader(
             structure_file_path=config_file,
@@ -50,15 +53,62 @@ class ChapterLoaderTest(BaseTestWithDB):
             chapter_loader.load
         )
 
+    def test_chapters_chapter_loader_missing_chapter_icon(self):
+        config_file = "basic-config.yaml"
+        chapter_slug = "chapter-1"
+        chapter_structure = {"number": 1}
+
+        chapter_loader = ChapterLoader(
+            structure_file_path=config_file,
+            chapter_slug=chapter_slug,
+            chapter_structure=chapter_structure,
+            BASE_PATH=self.base_path
+        )
+        self.assertRaises(
+            MissingRequiredFieldError,
+            chapter_loader.load
+        )
+
+    def test_chapters_chapter_loader_incorrect_interactive_slug(self):
+        config_file = "basic-config.yaml"
+        chapter_slug = "chapter-1"
+        chapter_structure = {
+            "number": 1,
+            "icon": "image.png",
+            "interactives": ["does-not-exist"]
+        }
+
+        chapter_loader = ChapterLoader(
+            structure_file_path=config_file,
+            chapter_slug=chapter_slug,
+            chapter_structure=chapter_structure,
+            BASE_PATH=self.base_path
+        )
+        self.assertRaises(
+            KeyNotFoundError,
+            chapter_loader.load
+        )
+
     def test_missing_heading(self):
-        """
-        """
-        pass
+        config_file = "basic-config.yaml"
+        chapter_slug = "missing-heading"
+        chapter_structure = {"number": 1, "icon": "image.png"}
+
+        chapter_loader = ChapterLoader(
+            structure_file_path=config_file,
+            chapter_slug=chapter_slug,
+            chapter_structure=chapter_structure,
+            BASE_PATH=self.base_path
+        )
+        self.assertRaises(
+            NoHeadingFoundInMarkdownFileError,
+            chapter_loader.load
+        )
 
     def test_chapters_chapter_loader_title_empty_content(self):
         config_file = "basic-config.yaml"
         chapter_slug = "missing-content"
-        chapter_structure = {"number": 1}
+        chapter_structure = {"number": 1, "icon": "image.png"}
 
         chapter_loader = ChapterLoader(
             structure_file_path=config_file,
@@ -74,7 +124,7 @@ class ChapterLoaderTest(BaseTestWithDB):
     def test_chapters_chapter_loader_empty_file(self):
         config_file = "basic-config.yaml"
         chapter_slug = "empty-file"
-        chapter_structure = {"number": 1}
+        chapter_structure = {"number": 1, "icon": "image.png"}
 
         chapter_loader = ChapterLoader(
             structure_file_path=config_file,
@@ -90,7 +140,7 @@ class ChapterLoaderTest(BaseTestWithDB):
     def test_chapters_chapter_loader_missing_markdown_file(self):
         config_file = "basic-config.yaml"
         chapter_slug = "this-file-does-not-exist"
-        chapter_structure = {"number": 1}
+        chapter_structure = {"number": 1, "icon": "image.png"}
 
         chapter_loader = ChapterLoader(
             structure_file_path=config_file,
