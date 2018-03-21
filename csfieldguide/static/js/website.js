624757b907834ff038264e6a5063da324f2e040c
Jack Morgan <jackmorgannz@gmail.com>
2018-03-21 15:28:55 +1300

    (Work in Progress) Begin writing templates and CSS for new design

--- csfieldguide/static/js/website.js
+++ csfieldguide/static/js/website.js
@@ -1,27 +1,15 @@
 $(document).ready(function(){
-  $(".button-collapse").sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  $('.scrollspy').scrollSpy();
-  $("#page-navigation").stick_in_parent();
+  // Display glossary-modal
+  $("#content-container, #glossary-modal").on("click", ".glossary-term", open_glossary_definition);
 
-  // Code for jumping to anchors
-  if (window.location.hash) {
-    var anchor = document.getElementById(window.location.hash.substring(1));
-    if (anchor && !window.location.hash.startsWith('#section-')) {
-      $target = $(anchor);
-      // Set target style on anchor
-      $target.addClass("glossary-anchor-link-highlight");
-      var $panel = $target.closest("li.panel-selector").children()[0];
-      // If within panel, open panel
-      if ($panel) {
-        $panel.click();
-      }
-    }
-  }
+  // Scrollspy for sidebar table of contents
+  $("body").scrollspy({ target: "#scrollspy-table-of-contents", offset: 140});
+
+  // If anchor link in URL, move page up to avoid link being covered by navbar
+  window.addEventListener("hashchange", function() { scrollBy(0, -72) })
+
+  // Pause YouTube videos playing within a closed details elements
+  $("body").on("click", "details[open]", details_element_closed);
 
   // Display an invitation to developers to contribute to this project
   console.log('%cAre you a developer?', 'font: bold 1.5em "Open Sans", sans-serif;')
@@ -30,30 +18,24 @@ $(document).ready(function(){
   console.log('%cWe would love your help to make this guide the best it can be!', 'font: 1.2em "Open Sans", sans-serif;')
   console.log('%cThanks,', 'font: 1.2em "Open Sans", sans-serif;')
   console.log('%cThe CSFG team :)', 'font: 1.2em "Open Sans", sans-serif;')
-
-  // Glossary stuff
-  $("body").on("click", ".glossary-term", open_glossary_definition);
 });
-
-  // Activate modal buttons
-  $('.modal-trigger').leanModal();
 
 function open_glossary_definition() {
   /**
    * Retrieve glossary definition.
    */
   var glossary_modal = $("#glossary-modal");
-  glossary_modal.openModal();
+  if (!glossary_modal.hasClass('show')) {
+    glossary_modal.modal('show');
+  }
 
   var slug = $(this).data("glossary-term");
   if (glossary_modal.attr("data-glossary-term") != slug) {
-    // TODO: Allow code to work for different languages
     $("#glossary-modal-term").text("Loading glossary definition...");
     $("#glossary-modal-definition").html("");
-    var url = "/chapters/glossary/json/";
     $.ajax({
       type: "GET",
-      url: url,
+      url: glossary_url,
       data: "term=" + slug,
       async: true,
       cache: true,
@@ -73,6 +55,11 @@ function update_glossary_modal(data) {
   glossary_modal.attr("data-glossary-term", data.slug);
   $("#glossary-modal-term").text(data.term);
   $("#glossary-modal-definition").html(data.definition);
+  if (data.translated) {
+    $("#glossary-modal-translation-unavailable").css({ "display": "none"});
+  } else {
+    $("#glossary-modal-translation-unavailable").css({ "display": "block"});
+  }
 }
 
 function show_glossary_modal_error(jqXHR, text_status, error_thrown) {
@@ -85,4 +72,32 @@ function show_glossary_modal_error(jqXHR, text_status, error_thrown) {
   var glossary_modal = $("#glossary-modal");
   glossary_modal.attr("data-glossary-term", "");
   $("#glossary-modal-term").text("Error!");
+}
+
+function details_element_closed() {
+  /**
+   * Pause any YouTube videos playing within the closed details element.
+   */
+  $('iframe[src*="youtube"]', this).each(function() {
+    var player = $(this).data('youtube-player');
+    if (player === undefined) {
+      // If iframe has no ID
+      if (!this.id) {
+        var src = this.src;
+        var video_id = src.substring(src.lastIndexOf('/') + 1, src.indexOf('?'));
+        this.id = 'youtube-embed-' + video_id;
+      }
+      // Create YouTube player for iframe
+      player = new YT.Player(this.id, {
+        events: {
+          'onReady': function (event) {
+            event.target.pauseVideo();
+          },
+        }
+      });
+      $(this).data('youtube-player', player);
+    } else {
+      player.pauseVideo();
+    }
+  });
 }
