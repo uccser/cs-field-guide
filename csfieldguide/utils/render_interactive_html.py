1e74803e4da861541377e19eb3efd655dab7cbe8
Jack Morgan <jackmorgannz@gmail.com>
2018-03-22 15:19:30 +1300

    Render interactives with request object

--- csfieldguide/utils/render_interactive_html.py
+++ csfieldguide/utils/render_interactive_html.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from interactives.models import Interactive
+from django.template.context_processors import i18n
 
 ALLOWED_MODES = (
     "in-page",
@@ -13,12 +14,13 @@ ALLOWED_MODES = (
 )
 
 
-def render_interactive_html(interactive_slug, mode):
+def render_interactive_html(interactive_slug, mode, request=None):
     """Return the HTML for an interactive.
 
     Args:
         interactive_slug (str): Slug of interactive.
         mode (str): Mode interactive should be rendered in.
+        request (Request): Request object to render template with.
 
     Returns:
         Rendered string of HTML.
@@ -33,7 +35,8 @@ def render_interactive_html(interactive_slug, mode):
             "interactive": interactive,
             "interactive_mode_template": mode_template,
         }
+        # context.update(i18n(None))
     else:
         # Flesh out this error checking
         raise Exception("Interactive must be one of the following modes...")
-    return render_to_string(interactive.template, context)
+    return render_to_string(interactive.template, context, request=request)
