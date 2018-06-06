f184a6455c920ef9b4f2e0cf8d6780d4d417cd4a
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-06 20:11:43 +1200

    Refactor the close window interactive.

--- csfieldguide/static/interactives/close-window/js/close-window.js
+++ csfieldguide/static/interactives/close-window/js/close-window.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $("#close-window-yes-button").click(function(){
         var body = $( "body" );
         body.empty();
-        body.addClass('center-align');
+        body.addClass('text-center');
         body.append('<h1>Why did you do that?!</h1>');
-        body.append('<a onclick="location.reload();" class="waves-effect waves-light btn btn-large">Refresh the page</a>');
+        body.append('<button onclick="location.reload();" class="btn btn-primary btn-lg">Refresh the page</button>');
     });
 });
