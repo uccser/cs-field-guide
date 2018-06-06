593b7b1420adba61de9bea117a575e46468ff186
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-07 11:59:56 +1200

    Make text in JavaScript file translatable.

--- csfieldguide/static/interactives/close-window/js/close-window.js
+++ csfieldguide/static/interactives/close-window/js/close-window.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var body = $( "body" );
         body.empty();
         body.addClass('text-center');
-        body.append('<h1>Why did you do that?!</h1>');
-        body.append('<button onclick="location.reload();" class="btn btn-primary btn-lg">Refresh the page</button>');
+        body.append('<h1>' + close_window_i18n["feedback"] + '</h1>');
+        body.append('<button onclick="location.reload();" class="btn btn-primary btn-lg">' + close_window_i18n["refresh"] + '</button>');
     });
 });
