036ca5754162dc5325da81f5b16a3ed0d3e59840
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-12 12:25:25 +1200

    Refactor payment interface interactive.

--- csfieldguide/static/interactives/payment-interface/js/payment-interface.js
+++ csfieldguide/static/interactives/payment-interface/js/payment-interface.js
@@ -7,11 +7,11 @@ function updatePaymentInteractive() {
 	var $paymentInteractiveMessage = $("#interactive-payment-interface-message");
 
 	if (inputText.match(/^(\d+(\.\d{0,2})?)$/) != null && inputText.substring(0,1) != "0") {
-		$paymentInteractiveMessage.html('Valid amount.');
+		$paymentInteractiveMessage.html(payment_interface_il8n["valid"]);
 		$paymentInteractiveMessage.addClass('success');
 		$paymentInteractiveMessage.removeClass('error');
 	} else {
-		$paymentInteractiveMessage.html('Invalid amount. Decimals are allowed up to two places.');
+		$paymentInteractiveMessage.html(payment_interface_il8n["invalid"]);
 		$paymentInteractiveMessage.addClass('error');
 		$paymentInteractiveMessage.removeClass('success');
 	}
