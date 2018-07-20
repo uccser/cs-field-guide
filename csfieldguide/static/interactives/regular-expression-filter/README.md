b205811e0f3e918835313b0fd2115649ed10a7a0
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-07-20 21:22:53 +1200

    Refactor regular-expression-filter interactive. Yet to get progress bar to update.

--- csfieldguide/static/interactives/regular-expression-filter/README.md
+++ csfieldguide/static/interactives/regular-expression-filter/README.md
@@ -4,6 +4,8 @@
 
 This interactive is created for users to write regular expressions, to filter words from a given list. The list of words used is used from [atebits/Words](https://github.com/atebits/Words), an open source list of words available under a [CC0 1.0 Universal license](https://github.com/atebits/Words/blob/master/LICENSE) (the alpha of the interactive used a list from [Interociter bulletin board](http://www-01.sil.org/linguistics/wordlists/english/)).
 
+NOTE: The words contained in en-words.txt are not translated because the interactive is about string matching, thus it doesn't matter what language the words are in.
+
 
 ## Required Files
 
--- csfieldguide/static/interactives/regular-expression-filter/js/regular-expression-filter.js
+++ csfieldguide/static/interactives/regular-expression-filter/js/regular-expression-filter.js
@@ -46,7 +46,8 @@ function displayWords() {
 
 // Updates the word count
 function updateWordCount() {
-  var text = "Showing " + RegularExpressionFilter.displayed + " out of " + RegularExpressionFilter.words.length + " lower case words"
+  var text = reg_exp_filter_il8n["showing"] + RegularExpressionFilter.displayed + 
+            reg_exp_filter_il8n["out-of"] + RegularExpressionFilter.words.length + reg_exp_filter_il8n["words"]
   document.getElementById('interactive-regular-expression-filter-feedback').innerHTML = text;
 };
 
@@ -59,8 +60,8 @@ function readWords() {
   var request = new XMLHttpRequest();
   request.addEventListener('load', fileLoaded);
   request.addEventListener('progress', updateLoadingProgress);
-  request.addEventListener('error', function() { alert('This interactive must be viewed online to load cross origin text file. Sorry for the inconvenience.')});
-  request.open('get', base_url + 'en-words.txt');
+  request.addEventListener('error', function() { alert(reg_exp_filter_il8n["error"])});
+  request.open('get', words_file_location);
   request.send();
 };
 
@@ -70,7 +71,7 @@ function updateLoadingProgress(request) {
   if (request.lengthComputable) {
     // Calculates percent complete (as a whole number e.g. 80%)
     var completed = (request.loaded / request.total) * 100;
-    document.getElementById('interactive-regular-expression-filter-loading-progress-bar').style.width = completed + '%';
+    document.getElementById('interactive-regular-expression-filter-loading-progress-bar').val = completed;
   }
 };
 
