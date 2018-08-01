805be85aadd8b3a6b173ee7c0f137358a1770057
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-08-01 16:20:59 +1200

    internationalization.

--- csfieldguide/static/interactives/nfa-guesser/js/nfa-guesser.js
+++ csfieldguide/static/interactives/nfa-guesser/js/nfa-guesser.js
@@ -64,10 +64,10 @@ function checkAnswer() {
     });
     var result_element = document.getElementById('interactive-nfa-guesser-result');
     if (checked_states.equals(nfa_guesser.result)) {
-        result_element.innerHTML = 'Correct'
+        result_element.innerHTML = nfa_guesser_il8n["correct"]
         result_element.classList = 'valid'
     } else {
-        result_element.innerHTML = 'Incorrect'
+        result_element.innerHTML = nfa_guesser_il8n["incorrect"]
         result_element.classList = ''
     }
 }
