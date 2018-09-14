4e3f362b170b7006779709b4db3d58268d0f426c
Hayley van Waas <hayleyavw@gmail.com>
2018-09-14 16:20:36 +1200

    Started testing out functionality of dragula library

--- csfieldguide/static/interactives/sorting-algorithms/js/sorting-algorithms.js
+++ csfieldguide/static/interactives/sorting-algorithms/js/sorting-algorithms.js
@@ -1,1 +1,14 @@
 console.log('sorting algorithms');
+
+$(function() {
+	dragula([
+		document.getElementById('box-1'),
+		document.getElementById('box-2'),
+		document.getElementById('box-3'),
+		document.getElementById('box-4'),
+		document.getElementById('box-5'),
+		document.getElementById('box-6'),
+		document.getElementById('box-7'),
+		document.getElementById('box-8')
+	]);
+});
