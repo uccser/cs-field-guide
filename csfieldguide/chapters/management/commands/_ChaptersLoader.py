3340bacf59b1ac990ffcb0cd26cf9efafdb20b63
courtneycb <courtneybracefield86@gmail.com>
2018-09-14 15:22:00 +1200

    Fix syntax.

--- csfieldguide/chapters/management/commands/_ChaptersLoader.py
+++ csfieldguide/chapters/management/commands/_ChaptersLoader.py
@@ -62,7 +62,7 @@ class ChaptersLoader(TranslatableModelLoader):
             chapter_icon = chapter_icon[4:-4]
 
         video = chapter_structure.get("video", None)
-        if video not None and "vimeo" not in video:
+        if video is not None and "vimeo" not in video:
             raise ValueError("Video must be a Vimeo video.")
 
         # Create chapter object and save to the db
