6cf7b6e5959497d4a9353891a63909ada28e9304
Hayley van Waas <hayleyavw@gmail.com>
2018-09-16 19:42:46 +1200

    Figured out how to access source and target elements on drag

--- csfieldguide/static/interactives/sorting-algorithms/js/sorting-algorithms.js
+++ csfieldguide/static/interactives/sorting-algorithms/js/sorting-algorithms.js
@@ -1,7 +1,7 @@
 console.log('sorting algorithms');
 
 $(function() {
-	dragula([
+	var drake = dragula([
 		document.getElementById('box-1'),
 		document.getElementById('box-2'),
 		document.getElementById('box-3'),
@@ -10,5 +10,10 @@ $(function() {
 		document.getElementById('box-6'),
 		document.getElementById('box-7'),
 		document.getElementById('box-8')
-	]);
+	]
+	);
+	console.log(drake);
+	drake.on('drop', (target) => {
+		console.log(target);
+	});
 });
