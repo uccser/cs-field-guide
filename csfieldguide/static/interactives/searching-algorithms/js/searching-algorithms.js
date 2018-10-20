8e982802e39a9fedb773f034d7da2159f23c7669
Hayley van Waas <hayleyavw@gmail.com>
2018-10-21 09:21:42 +1300

    can now move from one level to the next

--- csfieldguide/static/interactives/searching-algorithms/js/searching-algorithms.js
+++ csfieldguide/static/interactives/searching-algorithms/js/searching-algorithms.js
@@ -1,30 +1,77 @@
 var starting_num_guesses;
 var num_guesses;
 var target;
-var complete = false;
 var start_level;
 var end_level;
 var current_level;
 var num_boxes;
 var sorted;
 
+var preset_levels = {
+	1: {
+		"num-boxes": 5,
+		"sorted": "Random",
+		"num-guesses": 6
+	},
+	2: {
+		"num-boxes": 9,
+		"sorted": "Random",
+		"num-guesses": 10
+	},
+	3: {
+		"num-boxes": 9,
+		"sorted": "Sorted",
+		"num-guesses": 5
+	},
+	4: {
+		"num-boxes": 25,
+		"sorted": "Sorted",
+		"num-guesses": 5
+	}
+}
+
 window.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('interactive-searching-algorithms-next-level').addEventListener("click", nextLevel);
 	var url_string = window.location.href;
-    // var url = new URL(url_string);
 	setInterfaceParameters(url_string);
-	console.log(num_boxes);
-	console.log(sorted);
+	setUpInterface();
+});
+
+
+function setInterfaceParameters(url_string) {
+	var url = new URL(url_string);
+	num_boxes = url.searchParams.get('num-boxes'); // get num boxes parameter
+    sorted = url.searchParams.get('sorted'); // get sorted/not sorted parameter
+    starting_num_guesses = url.searchParams.get('num-guesses'); // get num guesses parameter
+    num_guesses = starting_num_guesses;
+    start_level = url.searchParams.get('start-level');
+    end_level = url.searchParams.get('end-level');
+    current_level = start_level;
 
     if (sorted == 'true') {
     	sorted = gettext('Sorted');
     } else if (sorted == 'false') {
     	sorted = gettext('Random');
     }
+}
 
-    // fill in the rules
+
+function setUpInterface() {
+	// fill in the rules
 	document.getElementById('interactive-searching-algorithms-num-boxes').innerText = num_boxes;
 	document.getElementById('interactive-searching-algorithms-num-guesses').innerText = num_guesses;
-	document.getElementById('interactive-searching-algorithms-order').innerText = sorted
+	document.getElementById('interactive-searching-algorithms-order').innerText = sorted;
+	document.getElementById('interactive-searching-algorithms-num-guesses-used').innerText = 0;
+
+	var found_text = document.getElementById('interactive-searching-algorithms-found');
+	if (found_text.classList.contains('show-message')) {
+		found_text.classList.remove('show-message');
+		found_text.classList.add('hide-message');
+	}
+
+	var rules_div = document.getElementById('interactive-searching-algorithms-default-rules');
+	rules_div.classList.remove('hide-message');
+	rules_div.classList.add('show-message');
 
 	target = Math.floor(Math.random() * Math.floor(num_boxes));
 	// create box elements and assign random weights
@@ -60,22 +107,19 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 
 	document.getElementById('interactive-searching-algorithms-target').innerText = target;
-});
-
-function setInterfaceParameters(url_string) {
-	// console.log(url);
-	var url = new URL(url_string);
-	num_boxes = url.searchParams.get('num-boxes'); // get num boxes parameter
-    sorted = url.searchParams.get('sorted'); // get sorted/not sorted parameter
-    starting_num_guesses = url.searchParams.get('num-guesses'); // get num guesses parameter
-    num_guesses = starting_num_guesses;
-    start_level = url.searchParams.get('start-level');
-    current_level = url.searchParams.get('end-level');
-    current_level = start_level;
-
-	console.log(sorted);
-    console.log(num_boxes);
 }
+
+
+function setNextLevelParameters() {
+	// set the parameters for the next level
+	var level = parseInt(current_level) + 1;
+	num_boxes = preset_levels[level]['num-boxes']; // get num boxes parameter
+    sorted = preset_levels[level]['sorted']; // get sorted/not sorted parameter
+    starting_num_guesses = preset_levels[level]['num-guesses']; // get num guesses parameter
+    num_guesses = starting_num_guesses;
+    current_level = level;
+}
+
 
 function fadeBox(event) {
 	var clicked_img = event.srcElement;
@@ -84,28 +128,44 @@ function fadeBox(event) {
 	img_weight.classList.add('show');
 	decreaseGuessCount();
 	if (target == img_weight.innerText) {
-		console.log('win!');
-		complete = true;
-		// if current level < end level
-		// http://localhost:81/en/interactives/searching-algorithms/?num-boxes=7&num-guesses=6&order=random&level=1
-		// build query string and call function to get new parameters
 		// hide rules
+		document.getElementById('interactive-searching-algorithms-default-rules').classList.remove('show-message');
 		document.getElementById('interactive-searching-algorithms-default-rules').classList.add('hide-message');
 		// show winning message
 		var num_guesses_used = starting_num_guesses - num_guesses;
 		document.getElementById('interactive-searching-algorithms-found').classList.remove('hide-message');
 		document.getElementById('interactive-searching-algorithms-found').classList.add('show-message');
 		document.getElementById('interactive-searching-algorithms-num-guesses-used').innerText = num_guesses_used;
+		// show next button
+		if (current_level < end_level) {
+			document.getElementById('interactive-searching-algorithms-next-level').classList.remove('hide-message');
+			document.getElementById('interactive-searching-algorithms-next-level').classList.add('show-message');
+			
+		}
 	}
 	
 }
+
+
+function nextLevel() {
+	setNextLevelParameters();
+	// remove the existing boxes
+	var box_div = document.getElementById('interactive-searching-algorithms-boxes');
+	while (box_div.firstChild) {
+		box_div.removeChild(box_div.firstChild);
+	}
+	// set up the interface again with the new parameters
+	setUpInterface();
+}
+
 
 function decreaseGuessCount() {
 	num_guesses -= 1;
 	if (num_guesses == 0) { // if no more guesses then hide the rules and display no guesses left message
 		var default_rules = document.getElementById('interactive-searching-algorithms-default-rules');
-		var no_guesses_left_message = document.getElementById('interactive-searching-algorithms-no-guesses');
 		default_rules.classList.add('hide-message');
+		default_rules.classList.remove('show-message');
+		var no_guesses_left_message = document.getElementById('interactive-searching-algorithms-no-guesses');
 		no_guesses_left_message.classList.add('show-message');
 	} else {
 		document.getElementById('interactive-searching-algorithms-num-guesses').innerText = num_guesses;
