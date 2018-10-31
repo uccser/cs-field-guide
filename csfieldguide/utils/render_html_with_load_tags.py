8f8b603613da37dffa07c0c192d0d2a6be1837f0
Jack Morgan <jackmorgannz@gmail.com>
2018-10-31 21:16:35 +1300

    Set load tags to be on one line

--- csfieldguide/utils/render_html_with_load_tags.py
+++ csfieldguide/utils/render_html_with_load_tags.py
@@ -2,12 +2,12 @@
 
 from django import template
 
-LOAD_TAGS = """
-{% load static %}
-{% load i18n %}
-{% load render_interactive_in_page %}
-{% load render_interactive_link %}
-"""
+LOAD_TAGS = (
+    "{% load static %}"
+    "{% load i18n %}"
+    "{% load render_interactive_in_page %}"
+    "{% load render_interactive_link %}"
+)
 
 
 def render_html_with_load_tags(html, context=None):
