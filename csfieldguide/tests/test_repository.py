3b5468ed4c1372813e8638ed5838ff5ca8e1b7d6
Jack Morgan <jackmorgannz@gmail.com>
2018-11-06 19:21:20 +1300

    Style improvements

--- csfieldguide/tests/test_repository.py
+++ csfieldguide/tests/test_repository.py
@@ -6,11 +6,11 @@ import glob
 from django.test import SimpleTestCase
 
 
-
 class RepositoryTests(SimpleTestCase):
     """Test class for tests for other areas of the project repository."""
 
     def test_package_json_includes_all_modules(self):
+        """Check if all 'package.json' files are listed in the root 'package.json' file."""
         ROOT_PACKAGE_JSON = "package.json"
         PATH_PREFIX = "file:./"
         IGNORE_FILES = [
@@ -31,5 +31,8 @@ class RepositoryTests(SimpleTestCase):
                 self.assertIn(
                     os.path.dirname(filepath) + os.path.sep,
                     listed_files,
-                    msg="'{}' could not be found in top level '{}' file".format(PATH_PREFIX + package_path, ROOT_PACKAGE_JSON)
+                    msg="'{}' could not be found in top level '{}' file".format(
+                        PATH_PREFIX + package_path,
+                        ROOT_PACKAGE_JSON
+                    )
                 )
