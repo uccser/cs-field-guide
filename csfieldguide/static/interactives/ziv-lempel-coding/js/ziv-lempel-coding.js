deedb0e5f983cad0a1d006b3a8df60ac85362046
Hayley van Waas <hayleyavw@gmail.com>
2018-11-27 16:12:26 +1300

    fixed error where would only match to max of 2 characters

--- csfieldguide/static/interactives/ziv-lempel-coding/js/ziv-lempel-coding.js
+++ csfieldguide/static/interactives/ziv-lempel-coding/js/ziv-lempel-coding.js
@@ -47,7 +47,7 @@ function repeat() { // TODO rename
     var longest_match_offset;
     var longest_match_length = 0;
     var current_length_of_match = 0;
-        
+
     for (var i = 0; i < sliding_window.length; i++) {
         // get next character in sliding window
         sw_character = sliding_window[i];
@@ -56,10 +56,11 @@ function repeat() { // TODO rename
             // record the current position as the start of the match in the sw
             match_offset = i;
             current_length_of_match = 1;
+            var next_sw_character_index = i;
             
             for (var j = 1; j < string_to_match.length; j++) {
                 // work out what the next characters are    
-                var next_sw_character_index = match_offset + 1;
+                next_sw_character_index = next_sw_character_index + 1;
                 var next_sw_character = sliding_window[next_sw_character_index];
                 var next_search_character = string_to_match[j];
                 
@@ -81,7 +82,6 @@ function repeat() { // TODO rename
 
     // STEP 4 write result to output
     if (longest_match_length >= min_match_length) {
-    
         num_characters = longest_match_length;
         var matched_characters = string_to_match.splice(0, longest_match_length);
         for (var i = 0; i < longest_match_length; i++) {
@@ -89,9 +89,6 @@ function repeat() { // TODO rename
         }
         var reference = '(' + longest_match_offset + ', ' + longest_match_length + ')';
         encoded_message.push(reference);
-        // console.log(encoded_message);
-        // console.log(sliding_window);
-    
     } else {
         num_characters = 1;
         unencoded_symbol = string_to_match.splice(0, 1);
