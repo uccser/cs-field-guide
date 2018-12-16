89ec98ba11bdb6d2e8042e57d43204ae6a1dd042
Hayley van Waas <hayleyavw@gmail.com>
2018-12-17 10:40:46 +1300

    codes now outputted along with dictionary

--- csfieldguide/static/interactives/lzw-coding/js/lzw-coding.js
+++ csfieldguide/static/interactives/lzw-coding/js/lzw-coding.js
@@ -1,8 +1,16 @@
+var output = [];
+var codes = [];
+
 function compress() {
-	var string_to_encode = "IAMSAMSAMIAM";
-	var codes = [];
+	compressText();
+	outputEncodedMessage();
+	outputDictionary();
+}
+
+function compressText() {
+	// var string_to_encode = 'IAMSAMSAMIAM';
+	var string_to_encode = document.getElementById('interactive-lzw-coding-input').value;
 	var current_sequence = "";
-	var output = [];
 
 	// set up initial dictionary
 	for (var i = 0; i < string_to_encode.length; i++) {
@@ -27,7 +35,38 @@ function compress() {
 		}
 	}
 
-	console.log(codes);
-	console.log(output);
+}
 
+function outputEncodedMessage() {
+	var encoded_message_div = document.getElementById('interactive-lzw-coding-encoded-text');
+	var fragment = document.createDocumentFragment();
+	for (var i = 0; i < output.length; i++) {
+		var placeholder_input = document.createElement('input');
+		placeholder_input.classList.add('interactive-lzw-placeholder-box');
+		placeholder_input.value = output[i];
+		fragment.appendChild(placeholder_input);
+	}
+	encoded_message_div.appendChild(fragment);
+}
+
+function outputDictionary() {
+	var dictionary_div = document.getElementById('interactive-lzw-coding-dictionary');
+	var fragment = document.createDocumentFragment();
+	for (var i = 0; i < codes.length; i++) {
+		var dictionary_code = document.createElement('div');
+		dictionary_code.classList.add('interactive-lzw-dictionary-entry');
+		
+		var code_element = document.createElement('p');
+		code_element.innerHTML = i;
+		code_element.classList.add('interactive-lzw-dictionary-code');
+
+		var value_element = document.createElement('p');
+		value_element.innerHTML = codes[i];
+		value_element.classList.add('interactive-lzw-dictionary-value');
+		
+		dictionary_code.appendChild(code_element);
+		dictionary_code.appendChild(value_element);
+		fragment.append(dictionary_code);
+	}
+	dictionary_div.appendChild(fragment);
 }
