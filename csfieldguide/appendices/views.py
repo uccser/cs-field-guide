03b750be196bd403b8b9bbf10a76f83af5ce79e5
courtneycb <courtneybracefield86@gmail.com>
2019-02-14 11:57:50 +1300

    Add Chapters to sitemap.

--- csfieldguide/appendices/views.py
+++ csfieldguide/appendices/views.py
@@ -1,7 +1,7 @@
 """Views for the appendices application."""
 
 from django.views.generic import TemplateView
-
+from chapters.models import Chapter
 
 class AppendicesList(TemplateView):
     """View for the list of appendices."""
@@ -43,3 +43,13 @@ class SitemapView(TemplateView):
     """View for the about page."""
 
     template_name = "appendices/sitemap.html"
+
+    def get_context_data(self, **kwargs):
+        """Provide the context data for the chapter view.
+
+        Returns:
+            Dictionary of context data.
+        """
+        context = super(SitemapView, self).get_context_data(**kwargs)
+        context["chapters"] = Chapter.objects.all()
+        return context
