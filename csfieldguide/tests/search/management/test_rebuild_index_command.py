59ab1ad43a7f38131130f656f6f719a680b10b6b
courtneycb <courtneybracefield86@gmail.com>
2019-02-14 11:25:40 +1300

    Update tests that refer to appendices models.

--- csfieldguide/tests/search/management/test_rebuild_index_command.py
+++ csfieldguide/tests/search/management/test_rebuild_index_command.py
@@ -10,7 +10,6 @@ from django.test import tag
 from django.template.exceptions import TemplateSyntaxError
 from tests.BaseTestWithDB import BaseTestWithDB
 from tests.chapters.ChaptersTestDataGenerator import ChaptersTestDataGenerator
-from tests.appendices.AppendicesTestDataGenerator import AppendicesTestDataGenerator
 
 BASE_PATH = "tests/search/management/"
 test_template_settings = settings.TEMPLATES
@@ -26,7 +25,6 @@ class ManagementCommandTest(BaseTestWithDB):
         super().__init__(*args, **kwargs)
         self.language = "en"
         self.chapters_test_data = ChaptersTestDataGenerator()
-        self.appendices_test_data = AppendicesTestDataGenerator()
 
     def setUp(self):
         """Automatically called before each test."""
@@ -49,33 +47,3 @@ class ManagementCommandTest(BaseTestWithDB):
         chapter = self.chapters_test_data.create_chapter(1)
         self.chapters_test_data.create_chapter_section(chapter, 1)
         management.call_command("rebuild_index", "--noinput")
-
-    def test_rebuild_index_command_appendix_model(self):
-        self.appendices_test_data.create_appendix(1, "appendix-1.html")
-        management.call_command("rebuild_index", "--noinput")
-
-    def test_rebuild_index_command_subappendix_model(self):
-        appendix = self.appendices_test_data.create_appendix(1, "appendix-1.html")
-        self.appendices_test_data.create_subappendix(appendix, 1, "appendix-1.html")
-        management.call_command("rebuild_index", "--noinput")
-
-    @override_settings(TEMPLATES=test_template_settings)
-    def test_rebuild_index_command_appendix_with_invalid_template(self):
-        self.appendices_test_data.create_appendix(1, "appendix-without-id.html")
-        self.assertRaises(
-            TemplateSyntaxError,
-            management.call_command,
-            "rebuild_index",
-            "--noinput"
-        )
-
-    @override_settings(TEMPLATES=test_template_settings)
-    def test_rebuild_index_command_subappendix_with_invalid_template(self):
-        appendix = self.appendices_test_data.create_appendix(1, "appendix-1.html")
-        self.appendices_test_data.create_subappendix(appendix, 1, "appendix-without-id.html")
-        self.assertRaises(
-            TemplateSyntaxError,
-            management.call_command,
-            "rebuild_index",
-            "--noinput"
-        )
