3a9c3d2fa6ef268bdadfcafe8a0b377150a263ad
eAlasdair <eAlasdair@gmail.com>
2019-03-29 13:52:41 +1300

    no the score system hasn't been reimplemented, remove for now]

--- csfieldguide/static/interactives/packet-attack/js/info.js
+++ csfieldguide/static/interactives/packet-attack/js/info.js
@@ -126,9 +126,7 @@ class Information extends Phaser.Scene {
             text = "Welcome to Packet Attack. In this game, your job is to attack the packet creatures and stop messages being delivered.\nYou pass a level if the received message is any different from the one sent.\nWhile a creature is in the danger zone (indicated by the yellow and grey area) you may attack by clicking the command buttons.\nGood luck!"
             break;
           case InfoPaneType.END:
-              text = "This level is actually impossible to beat\nThe system can handle anything you throw at it!\nThank you for playing\nTotal Score: " + this.registry.get('score') + "\n\n\n\nPress start if you want to try this level again";
-              //this.button.visible = false;
-              //this.game.score = 0; //Reset score.
+              text = "This level is actually impossible to beat\nThe system can handle anything you throw at it!\nThank you for playing\n\n\n\n\nPress start if you want to try this level again";
               break;
         }
         return text;
