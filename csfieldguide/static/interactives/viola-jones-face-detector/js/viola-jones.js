f616a6b27120971563600eb3d51b05b933cfead5
courtneycb <courtneybracefield86@gmail.com>
2019-05-07 10:12:19 +1200

    debugging

--- csfieldguide/static/interactives/viola-jones-face-detector/js/viola-jones.js
+++ csfieldguide/static/interactives/viola-jones-face-detector/js/viola-jones.js
@@ -1,5 +1,5 @@
 const interact = require("interactjs");
-const tracking = require("tracking");;
+const trackingjs = require("tracking");;
 
 // listen for HTML5 native drag and drop API dragstart event and ignore them.
 document.addEventListener('dragstart', function(event) {
@@ -287,7 +287,8 @@ function loadResizeImage(src) {
 
     drawGrayscaleImage(img);
     var myData = context.getImageData(0, 0, canvas.width, canvas.height);
-    tracking.Image.computeIntegralImage(myData.data, myData.width, myData.height, result);
+    console.log(trackingjs);
+    trackingjs.tracking.Image.computeIntegralImage(myData.data, myData.width, myData.height, result);
     clearRectangles();
   }
 }
@@ -322,9 +323,9 @@ function drawGrayscaleImage(img) {
  */
 function findFaces() {
   var img = document.getElementById('img');
-  var tracker = new tracking.ObjectTracker(['face']);
+  var tracker = new trackingjs.tracking.ObjectTracker(['face']);
   tracker.setStepSize(2);
-  trackerTask = tracking.track('#img', tracker);
+  trackerTask = trackingjs.tracking.track('#img', tracker);
   tracker.on('track', function(event) {
     event.data.forEach(function(rect) {
       var faceRect = document.createElement('div');
