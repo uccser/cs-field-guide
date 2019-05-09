ace2bb84d7b821b4720646b457ac201ba36a76f3
eAlasdair <eAlasdair@gmail.com>
2019-05-09 15:56:23 +1200

    add top two cyclic arrows

--- csfieldguide/static/interactives/iterative-software-development/js/iterative-software-development.js
+++ csfieldguide/static/interactives/iterative-software-development/js/iterative-software-development.js
@@ -5,3 +5,79 @@
  * and:
  * https://stackoverflow.com/questions/20037122/draw-an-arrow-between-two-divs
  */
+
+var analysisDiv;
+var designDiv;
+var implementationDiv;
+var testingDiv;
+var feedbackDiv;
+
+var analysisToDesignArrow;
+var designToImplementationArrow;
+var implementationToTestingArrow;
+var testingToAnalysisArrow;
+var feedbackArrow;
+
+$(document).ready(function() {
+  analysisDiv = $('#analysis');
+  designDiv = $('#design');
+  implementationDiv = $('#implementation');
+  testingDiv = $('#testing');
+  feedbackDiv = $('#feedback-text');
+
+  analysisToDesignArrow = $('#analysis-design');
+  designToImplementationArrow = $('#design-implementation');
+  implementationToTestingArrow = $('#implementation-testing');
+  testingToAnalysisArrow = $('#testing-analysis');
+  feedbackArrow = $('#feedback');
+
+  window.onresize = function() {
+    createArrow(analysisToDesignArrow, analysisDiv, designDiv, 1);
+    //createArrow(designToImplementationArrow, designDiv, implementationDiv, 2);
+    //createArrow(implementationToTestingArrow, implementationDiv, testingDiv, 3);
+    createArrow(testingToAnalysisArrow, testingDiv, analysisDiv, 4);
+
+    //createFeedbackArrow(feedbackArrow, feedbackDiv);
+  }
+});
+
+/**
+ * 
+ * Based on defining a 90 degree curve as a partial ellipse:
+ * Start: (x1,y1)
+ * Curve: (x2-x1,y2-y1) will make it have endpoints perpendicular to the boxes
+ * End: (x2,y2)
+ */
+function createArrow(arrow, from, to, rotation) {
+  var fromLocation = from.offset();
+  var toLocation = to.offset();
+
+  var line = "M";
+  var start = [0,0];
+  var curve = [0,0];
+  var end = [0,0];
+  var endpointOffset = [0,0]; // To account for the arrowhead
+  switch(rotation) {
+    case(1):
+      endpointOffset = [0, -15];
+      start = [(fromLocation.left + from.width()), (fromLocation.top + from.height() / 2)];
+      end = [(toLocation.left + to.width() / 2 + endpointOffset[0]), (toLocation.top + endpointOffset[1])];
+      break;
+    case(4):
+      endpointOffset = [-15, 0];
+      start = [(fromLocation.left + from.width() / 2), (fromLocation.top)];
+      end = [(toLocation.left + endpointOffset[0]), (toLocation.top + to.height() / 2 + endpointOffset[1])];
+      break;
+  }
+
+  curve = [end[0] - start[0], end[1] - start[1]];
+  line += start[0] + "," + start[1] + " A" + curve[0] + "," + curve[1];
+  line += " 0 0,1 "; // Boolean flags, constant
+  line += end[0] + "," + end[1];
+
+  arrow.attr('d', line);
+}
+
+function createFeedbackArrow() {
+
+}
