16315a835a8a6c7708ab7455f0a26eb9b42c23b3
eAlasdair <eAlasdair@gmail.com>
2019-05-20 10:25:54 +1200

    finish pyinterpreter

--- csfieldguide/static/interactives/python-interpreter/js/python-interpreter.js
+++ csfieldguide/static/interactives/python-interpreter/js/python-interpreter.js
@@ -15,14 +15,38 @@ $(document).ready(function () {
         if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
           throw "File not found: '" + x + "'";
         return Sk.builtinFiles["files"][x];
-        },
+      },
       output: skuplt_out,
       python3: true
-      });
+    });
+
     try {
       Sk.importMainWithBody("<stdin>", false, program);
     } catch (e) {
-      alert(e);
+      console.log(e);
+    }
+  });
+
+  // From https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+  // Edited for this use case
+  // Inserts two spaces instead of tabbing to the next element
+  $(document).delegate('#interactive-python-interpreter-code', 'keydown', function(key) {
+    if(key.which == 9) {
+      console.log('tab');
+      // Tab was pressed
+      key.preventDefault();
+
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+    
+      // Insert the 2 spaces
+      $(this).val($(this).val().substring(0, start)
+                + "  "
+                + $(this).val().substring(end));
+      
+      // Put the cursor bar | where the user expects
+      this.selectionStart = start + 2
+      this.selectionEnd = this.selectionStart;
     }
   });
 
@@ -39,6 +63,9 @@ $(document).ready(function () {
   programBox.value = printText;
 });
 
+/**
+ * Pushes the result of the python calculation to the output box
+ */
 function skuplt_out(text) {
   var output = document.getElementById("interactive-python-interpreter-output");
   text = text.replace(/</g, '&lt;');
