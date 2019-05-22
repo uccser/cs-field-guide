c457c1d08911385cdbfc0fc3aab440252f48057b
courtneycb <courtneybracefield86@gmail.com>
2019-05-22 17:27:05 +1200

    Style fixes.

--- csfieldguide/tests/appendices/urls/test_changelog_url.py
+++ csfieldguide/tests/appendices/urls/test_changelog_url.py
@@ -1,7 +1,6 @@
 from tests.BaseTestWithDB import BaseTestWithDB
 
 
-
 class ChangelogURLTest(BaseTestWithDB):
 
     def __init__(self, *args, **kwargs):
@@ -11,4 +10,8 @@ class ChangelogURLTest(BaseTestWithDB):
     def test_appendices_valid_changelog_url(self):
         url = "http://www.csfieldguide.org.nz/en/further-information/releases.html"
         response = self.client.get(url)
-        self.assertRedirects(response, "https://cs-field-guide.readthedocs.io/en/latest/changelog.html", fetch_redirect_response=False)
+        self.assertRedirects(
+            response,
+            "https://cs-field-guide.readthedocs.io/en/latest/changelog.html",
+            fetch_redirect_response=False
+        )
