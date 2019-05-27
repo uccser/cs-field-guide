4df1287e84045505398aa071ec472b7240036ac2
courtneycb <courtneybracefield86@gmail.com>
2019-05-28 09:57:39 +1200

    remove duplicate line.

--- csfieldguide/static/interactives/pixel-viewer/js/pixel-viewer.js
+++ csfieldguide/static/interactives/pixel-viewer/js/pixel-viewer.js
@@ -227,9 +227,8 @@ function Blur(parent_element){
   // Add a description about noise, then give opportunity for students to introduce noise.
   this.main_div.append($(document.createElement("p")).text(gettext("Sometimes images have noise, and applying a blur can be a helpful way to preprocess\
   an image that contains noise before using other Computer Vision algorithms. Use this to add some \"salt and pepper\" noise to the image and then\
-  observe what happens when you apply the blurs to a noisy image. Perhaps you have a noisy image that you could upload yourself?")))
-  .append($("<label></label>").text(gettext("Amount of noise to add (%): ")));
-
+  observe what happens when you apply the blurs to a noisy image. Perhaps you have a noisy image that you could upload yourself?")));
+  
   this.main_div.append($("<label></label>").text(gettext("Amount of noise to add (%): ")));
   this.main_div.append($(document.createElement("input"))
       .attr({"type": "number", "value": 10, "id" : "noise_selector", "class" : "form-control w-auto d-inline m-1 mt-2 percent_selector int_selector pos_int_selector"})
