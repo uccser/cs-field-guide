91353daa5ad7f11b7e275c109f70c15b6360c3d4
courtneycb <courtneybracefield86@gmail.com>
2019-06-05 09:37:34 +1200

    Allow users to input own number on rgb.

--- csfieldguide/static/interactives/rgb-mixer/js/rgb-mixer.js
+++ csfieldguide/static/interactives/rgb-mixer/js/rgb-mixer.js
@@ -33,6 +33,11 @@ $(document).ready(function () {
     	}
     });
   }
+
+  $('#interactive-rgb-mixer-red-value').on('input', RGB_Mixer.setColorFromInputBox);
+  $('#interactive-rgb-mixer-green-value').on('input', RGB_Mixer.setColorFromInputBox);
+  $('#interactive-rgb-mixer-blue-value').on('input', RGB_Mixer.setColorFromInputBox);
+
   // Bind the color changing function
   // to the update event. Fires immediately so has to be outside for-loop.
   RGB_Mixer.sliders[0].noUiSlider.on('update', RGB_Mixer.setColor);
@@ -56,7 +61,25 @@ RGB_Mixer.setColor = function(){
 	RGB_Mixer.result.style.color = color;
 
   // Set text for labels
-  $('#interactive-rgb-mixer-red-value').text(RGB_Mixer.sliders[0].noUiSlider.get());
-  $('#interactive-rgb-mixer-green-value').text(RGB_Mixer.sliders[1].noUiSlider.get());
-  $('#interactive-rgb-mixer-blue-value').text(RGB_Mixer.sliders[2].noUiSlider.get());
+  $('#interactive-rgb-mixer-red-value').val(RGB_Mixer.sliders[0].noUiSlider.get());
+  $('#interactive-rgb-mixer-green-value').val(RGB_Mixer.sliders[1].noUiSlider.get());
+  $('#interactive-rgb-mixer-blue-value').val(RGB_Mixer.sliders[2].noUiSlider.get());
+};
+
+RGB_Mixer.setColorFromInputBox = function(){
+  // Get the slider values,
+  red_val = $('#interactive-rgb-mixer-red-value').val();
+  green_val = $('#interactive-rgb-mixer-green-value').val();
+  blue_val = $('#interactive-rgb-mixer-blue-value').val();
+	// stick them together.
+	var color = 'rgb(' + red_val + ',' + green_val + ',' + blue_val + ')';
+
+	// Fill the color box.
+	RGB_Mixer.result.style.background = color;
+	RGB_Mixer.result.style.color = color;
+
+  // Set slider handle position
+  RGB_Mixer.sliders[0].noUiSlider.set(red_val);
+  RGB_Mixer.sliders[1].noUiSlider.set(green_val);
+  RGB_Mixer.sliders[2].noUiSlider.set(blue_val);
 };
