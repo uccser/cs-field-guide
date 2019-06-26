267d8e9de736b4d944d6692cec3a99c976259419
eAlasdair <eAlasdair@gmail.com>
2019-06-26 13:49:58 +1200

    add redirects from appendices application to curriculum_guides

--- csfieldguide/appendices/urls.py
+++ csfieldguide/appendices/urls.py
@@ -1,6 +1,7 @@
 """URL routing for the appendices application."""
 
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -25,5 +26,15 @@ urlpatterns = [
         r"^sitemap/$",
         views.SitemapView.as_view(),
         name="sitemap"
+    ),
+    # eg: redirect /appendices/curriculum-guides/ to /curriculum-guides/
+    url(
+        r"^curriculum-guides/$",
+        RedirectView.as_view(permanent=True, pattern_name="curriculum_guides:index")
+    ),
+    # eg: redirect /appendices/curriculum-guides/ncea/ to /curriculum-guides/ncea/
+    url(
+        r"^curriculum-guides/(?P<curriculum_guide_slug>[-\w]+)/$",
+        RedirectView.as_view(permanent=True, pattern_name="curriculum_guides:curriculum_guide")
     ),
 ]
