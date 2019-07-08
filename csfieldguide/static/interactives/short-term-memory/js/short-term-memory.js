64c2790fb4f4a5673ee11d85b19743903d7d6ea0
courtneycb <courtneybracefield86@gmail.com>
2019-07-08 15:39:07 +1200

    Validation. WIP.

--- csfieldguide/static/interactives/short-term-memory/js/short-term-memory.js
+++ csfieldguide/static/interactives/short-term-memory/js/short-term-memory.js
@@ -1,3 +1,20 @@
+var itemsShown = [
+  "banana",
+  "bicycle",
+  "book",
+  "cactus",
+  "calculator",
+  "camera",
+  "duck",
+  "toothbrush",
+  "forklift",
+  "guitar",
+  "scissors",
+  "soccerball",
+  "sock",
+  "teapot"
+]
+
 $(document).ready(function(){
   $('#ready-button').click(function() {
     $('#items-container').removeClass('d-none');
@@ -23,5 +40,42 @@ $(document).ready(function(){
         timerText.html(timeLeft);
       }
     }
+  });
+
+  $('#submit-button').click(function() {
+    var answer = $('#stm-answer-input').val();
+    // Remove whitespace
+    answer = answer.replace(/\s/g, '');
+    answerList = answer.split(',');
+
+    var correct = 0;
+    for (i = 0; i < answerList.length; i++) {
+      if (itemsShown.includes(answerList[i])) {
+        correct += 1;
+      }
+    }
+
+    $('#num-correct').html(correct);
+    $('#answer-input').addClass('d-none');
+
+    answerSet = new Set(answerList);
+    itemsShownSet = new Set(itemsShown);
+    itemsMissedSet = new Set([...itemsShownSet].filter(x => !answerSet.has(x)));
+
+    itemsMissed = '';
+    itemsMissedSet.forEach(function(value) {
+      itemsMissed += value + '<br>';  
+    })
+
+    $('#items-missed').html(itemsMissed);
+    $('#completion-message').removeClass('d-none');
+    $('#restart-button').removeClass('d-none');
+  });
+
+  $('#restart-button').click(function() {
+    $('.intro-content').removeClass('d-none');
+    $('#stm-timer b').css('color', '#212529');
+    $('#completion-message').addClass('d-none');
+    $('#restart-button').addClass('d-none');
   });
 });
