3c93b423d02c28b52fecdf0d4ef87e6490609461
eAlasdair <eAlasdair@gmail.com>
2019-07-25 11:47:30 +1200

    add main heading to table

--- csfieldguide/static/interactives/training-ground/js/html-table.js
+++ csfieldguide/static/interactives/training-ground/js/html-table.js
@@ -1,5 +1,6 @@
 var TXT_REMAINING = gettext("Sticks<br>remaining:");
 var TXT_CHANCE = gettext("Chance AI picks (%):");
+var TXT_NETWORK = gettext("Nathaniel's neural network:");
 var TXT_STICKS = {
   '1': gettext("1 stick"),
   '2': gettext("2 sticks"),
@@ -22,25 +23,26 @@ class HtmlTable {
 
   createTable(size) {
     this.rows = size;
-    var baseTable = '<table>\n';
-    baseTable +=    '  <tr>\n'
-              +     '    <th rowspan="2">' + TXT_REMAINING + '</th>\n'
-              +     '    <th colspan="3">' + TXT_CHANCE + '</th>\n'
-              +     '  </tr>\n'
-              +     '  <tr>\n'
-              +     '    <th>' + TXT_STICKS['1'] + '</th>\n'
-              +     '    <th>' + TXT_STICKS['2'] + '</th>\n'
-              +     '    <th>' + TXT_STICKS['3'] + '</th>\n'
-              +     '  </tr>\n';
+    var baseTable = '<tr>\n';
+    baseTable +=    '  <th colspan="4">' + TXT_NETWORK + '</th>\n'
+              +     '</tr>\n'
+              +     '<tr>\n'
+              +     '  <th rowspan="2">' + TXT_REMAINING + '</th>\n'
+              +     '  <th colspan="3">' + TXT_CHANCE + '</th>\n'
+              +     '</tr>\n'
+              +     '<tr>\n'
+              +     '  <th>' + TXT_STICKS['1'] + '</th>\n'
+              +     '  <th>' + TXT_STICKS['2'] + '</th>\n'
+              +     '  <th>' + TXT_STICKS['3'] + '</th>\n'
+              +     '</tr>\n';
     for (var i=this.rows; i > 0; i--) {
-      baseTable +=  '  <tr id="row-' + i + '">\n'
-                +   '    <td id="' + i + '-remaining">' + i + '</td>\n'
-                +   '    <td id="' + i + '-1-sticks">N/A</td>\n'
-                +   '    <td id="' + i + '-2-sticks">N/A</td>\n'
-                +   '    <td id="' + i + '-3-sticks">N/A</td>\n'
-                +   '  </tr>\n';
+      baseTable +=  '<tr id="row-' + i + '">\n'
+                +   '  <td id="' + i + '-remaining">' + i + '</td>\n'
+                +   '  <td id="' + i + '-1-sticks">N/A</td>\n'
+                +   '  <td id="' + i + '-2-sticks">N/A</td>\n'
+                +   '  <td id="' + i + '-3-sticks">N/A</td>\n'
+                +   '</tr>\n';
     }
-    baseTable +=    '</table>\n';
     this.$parent.html(baseTable);
   }
 
