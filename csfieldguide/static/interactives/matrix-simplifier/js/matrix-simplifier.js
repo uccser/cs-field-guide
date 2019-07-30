5b38b766e946134a5842f7b7ee6e56f644e868c9
courtneycb <courtneybracefield86@gmail.com>
2019-07-30 13:41:15 +1200

    Allow user to add their own matrix.

--- csfieldguide/static/interactives/matrix-simplifier/js/matrix-simplifier.js
+++ csfieldguide/static/interactives/matrix-simplifier/js/matrix-simplifier.js
@@ -1,4 +1,9 @@
 const mathjs = require('mathjs');
+const sprintf = require('sprintf-js').sprintf;
+const vsprintf = require('sprintf-js').vsprintf;
+
+const ROW_TEMPLATE = "%s & %s & %s";
+const MATRIX_TEMPLATE = "\\begin{bmatrix} %s \\\\ %s \\\\ %s \\end{bmatrix}";
 
 $(document).ready(function() {
   $('#add-matrix-from-input').click(addMatrix);
@@ -9,7 +14,12 @@ function addMatrix() {
   matrixArray = getMatrix();
   matrix = mathjs.matrix(matrixArray);
   console.log(matrix);
+  matrixString = formatMatrix(matrixArray);
+  $('#matrix-1').html(matrixString);
+  // Force re-render
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'matrix-1']);
 }
+
 
 function getMatrix() {
   row0 = [
@@ -31,6 +41,15 @@ function getMatrix() {
   ];
 
   return [row0, row1, row2];
+}
+
+
+function formatMatrix(matrix) {
+  row0 = vsprintf(ROW_TEMPLATE, matrix[0]);
+  row1 = vsprintf(ROW_TEMPLATE, matrix[1]);
+  row2 = vsprintf(ROW_TEMPLATE, matrix[2]);
+
+  return sprintf(MATRIX_TEMPLATE, row0, row1, row2);
 }
 
 
