e8cdee00660f971c813457c55c254ae789c428e5
eAlasdair <ealasdair@gmail.com>
2019-08-21 16:48:04 +1200

    more passwords, update functionality

--- csfieldguide/static/interactives/password-guesser/js/password-guesser.js
+++ csfieldguide/static/interactives/password-guesser/js/password-guesser.js
@@ -1,29 +1,98 @@
+var urlParameters = require('../../../js/third-party/url-parameters.js');
+
 const SHA256 = require('crypto-js/sha256');
 const Hex = require('crypto-js/enc-hex');
+const Passwords = require('./passwords.js');
 
-var hashTable = {
-  '#row_A': '',
-  '#row_B': '',
-  '#row_C': '',
-  '#row_D': '',
-  '#row_E': '',
-  '#row_F': ''
-}
+// List indices:
+// [name, the password or encryption , salt, [encryption with salt]]
+const NAME = 0;
+const HASH = 1;
+const SALT = 2;
+const HASHWITHSALT = 3;
+
+var isSalted;
+var allPasswords = [];
 
 $(document).ready(function() {
-  for (var row in hashTable) {
-    hashTable[row] = $(row).children('.hash').html().replace(/\s/g, '');
+  isSalted = (urlParameters.getUrlParameter('salted') == "true");
+  if (isSalted) {
+    $('#salt-header').removeClass('d-none');
+    $('#salt-input').removeClass('d-none');
   }
+  $('#pg-password-salt').val('');
+  $('#pg-password-guess').val('');
 
-  $('#pg-submit-button').click(function() {
-    checkGuess();
-  });
+  setUpTable();
+
+  $('#pg-submit-button').click(checkGuess);
 
   $('.pg-img').click(function() {
     var salt = $(this).parent().html().trim().substring(0, 16); // Get the 16 character salt
     $('#pg-password-salt').val(salt);
   })
 });
+
+function setUpTable() {
+  hashLanguagePasswords();
+
+  var i = 0;
+  var j = 0;
+  var x = Passwords.LanguagePasswords.length;
+  while (i < x) {
+    addToTable(Passwords.LanguagePasswords[i], j);
+    i++;
+    j++;
+  }
+  i = 0;
+  x = Passwords.WordPasswords.length;
+  while (i < x) {
+    addToTable(Passwords.WordPasswords[i], j);
+    i++;
+    j++;
+  }
+  i = 0;
+  x = Passwords.SentencePasswords.length;
+  while (i < x) {
+    addToTable(Passwords.SentencePasswords[i], j);
+    i++;
+    j++;
+  }
+}
+
+function hashLanguagePasswords() {
+  var password;
+  var hashWithSalt;
+  var hashNoSalt;
+  for (var i=0; i < Passwords.LanguagePasswords.length; i++) {
+    password = Passwords.LanguagePasswords[i];
+    hashWithSalt = SHA256(password[HASH] + password[SALT]).toString(Hex).toUpperCase();
+    hashNoSalt = SHA256(password[HASH]).toString(Hex).toUpperCase();
+    Passwords.LanguagePasswords[i] = [password[NAME], hashNoSalt, password[SALT], hashWithSalt];
+  }
+}
+
+function addToTable(password, index) {
+  var $table = $('#password-table');
+  console.log(index);
+  var html =  '<tr id="row-' + index + '">\n' +
+              '  <td>\n' +
+              '    ' + password[NAME] + '\n' +
+              '  </td>\n';
+  if (isSalted) {
+    html +=   '  <td class="salt">\n' +
+              '    ' + password[SALT] + '\n' +
+              '    <img class="pg-img" src="https://img.icons8.com/material-rounded/24/000000/copy.png">\n' +
+              '  </td>\n';
+  }
+  html +=     '  <td class="hash">\n' +
+              '    ' + (isSalted ? password[HASHWITHSALT] : password[HASH]) + '\n' +
+              '  </td>\n' +
+              '</tr>\n';
+  
+  allPasswords.push(password);
+  $table.append(html);
+}
 
 /**
  * Concatenates the guessed password with the given salt, hashes the result,
@@ -36,16 +105,27 @@ function checkGuess() {
   unhighlight('#pg-calculated-hash');
 
   var guess = $('#pg-password-guess').val().replace(/\s/g, '');
-  var salt = $('#pg-password-salt').val().replace(/\s/g, '');
-  var hash = SHA256(guess + salt).toString(Hex).toUpperCase();
+  var salt;
+  var hash;
+  if (isSalted) {
+    salt = $('#pg-password-salt').val().replace(/\s/g, '');
+    hash = SHA256(guess + salt).toString(Hex).toUpperCase();
+  } else {
+    hash = SHA256(guess).toString(Hex).toUpperCase();
+  }
   $('#pg-calculated-hash').html(hash);
 
-  for (var row in hashTable) {
-    if (hashTable[row] == hash) {
+  var row;
+  var password;
+  for (var i=0; i < allPasswords.length; i++) {
+    password = allPasswords[i];
+    row = '#row-' + i;
+    console.log(row);
+    if ((isSalted && password[HASHWITHSALT] == hash) || (!isSalted && password[HASH] == hash)) {
       highlight('#pg-calculated-hash');
       highlight(row);
     } else {
-      unhighlight (row);
+      unhighlight(row);
     }
   }
 }
