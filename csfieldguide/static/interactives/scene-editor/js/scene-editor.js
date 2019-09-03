c644e4554c48a603d9c8b91f33a702d0d7773109
courtneycb <courtneybracefield86@gmail.com>
2019-09-04 10:58:51 +1200

    WIP

--- csfieldguide/static/interactives/scene-editor/js/scene-editor.js
+++ csfieldguide/static/interactives/scene-editor/js/scene-editor.js
@@ -1,18 +1,26 @@
 var THREE = require('three');
+var detector = require('../../../js/third-party/threejs/Detector.js');
+
+// check that the browser is webgl compatible
+if (! detector.Detector.webgl) detector.Detector.addGetWebGLMessage();
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var container = document.getElementById('container');
+container.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 
 camera.position.z = 5;
+camera.position.y = 120 * Math.PI / 180;
 
 var animate = function () {
   requestAnimationFrame( animate );
