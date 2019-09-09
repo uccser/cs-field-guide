cfcb828b5f212119e82b4ba0ceaf8cce283313e9
courtneycb <courtneybracefield86@gmail.com>
2019-09-09 15:04:08 +1200

    Add sky box and axes... change EVERYTHING!

--- csfieldguide/static/interactives/scene-editor/js/scene-editor.js
+++ csfieldguide/static/interactives/scene-editor/js/scene-editor.js
@@ -1,47 +1,185 @@
-var THREE = require('three');
-var detector = require('../../../js/third-party/threejs/Detector.js');
+const THREE = require('three');
+const OrbitControls = require('three-orbit-controls')(THREE);
+const detector = require('../../../js/third-party/threejs/Detector.js');
+
+const image_base_path = base_static_path + 'interactives/scene-editor/img/bridge-';
+var controls, camera, scene, renderer;
+var cameraCube, sceneCube;
+var textureCube;
+var cubeMesh;
 
 // check that the browser is webgl compatible
 if (! detector.Detector.webgl) detector.Detector.addGetWebGLMessage();
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 15000 );
-camera.position.x = 100;
-camera.position.y = 100;
-camera.position.z = 500;
-camera.lookAt(new THREE.Vector3(0,0,0));
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-var container = document.getElementById('scene');
-container.appendChild( renderer.domElement );
-
-var geometry = new THREE.CubeGeometry( 100, 100, 100 );
-var material = new THREE.MeshLambertMaterial( {color: 0xeb4034} );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-var light = new THREE.PointLight( 0xff2200 );
-light.position.set( 100, 100, 100 );
-scene.add( light );
-var light = new THREE.AmbientLight( 0x111111 );
-scene.add( light );
-
-var size = 1000;
-var divisions = 10;
-var gridHelper = new THREE.GridHelper( size, divisions );
-scene.add( gridHelper );
-
-var axesHelper = new THREE.AxesHelper( 500 );
-scene.add( axesHelper );
-
-var animate = function () {
-  requestAnimationFrame( animate );
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-};
-
+init();
 animate();
+
+function init() {
+  // Cameras
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 100000 );
+  camera.position.set( 0, 0, 1000 );
+  cameraCube = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 100000 );
+  // Scene
+  scene = new THREE.Scene();
+  sceneCube = new THREE.Scene();
+  // Lights
+  var ambient = new THREE.AmbientLight( 0xffffff );
+  scene.add( ambient );
+  // Textures
+  var urls = [ 
+    image_base_path + "posx.jpg", image_base_path + "negx.jpg",
+    image_base_path + "posy.jpg", image_base_path + "negy.jpg",
+    image_base_path + "posz.jpg", image_base_path + "negz.jpg" 
+  ];
+  textureCube = new THREE.CubeTextureLoader().load( urls );
+  textureCube.format = THREE.RGBFormat;
+  textureCube.mapping = THREE.CubeReflectionMapping;
+  textureCube.encoding = THREE.sRGBEncoding;
+
+  var cubeShader = THREE.ShaderLib[ "cube" ];
+  var cubeMaterial = new THREE.ShaderMaterial( {
+    fragmentShader: cubeShader.fragmentShader,
+    vertexShader: cubeShader.vertexShader,
+    uniforms: cubeShader.uniforms,
+    depthWrite: false,
+    side: THREE.BackSide
+  } );
+  cubeMaterial.uniforms[ "tCube" ].value = textureCube;
+  Object.defineProperty( cubeMaterial, 'map', {
+    get: function () {
+      return this.uniforms.tCube.value;
+    }
+  } );
+  // Skybox
+  cubeMesh = new THREE.Mesh( new THREE.BoxBufferGeometry( 100, 100, 100 ), cubeMaterial );
+  sceneCube.add( cubeMesh );
+  //
+  var geometry = new THREE.SphereBufferGeometry( 200.0, 48, 24 );
+  sphereMaterial = new THREE.MeshLambertMaterial( { envMap: textureCube } );
+  sphereMesh = new THREE.Mesh( geometry, sphereMaterial );
+  scene.add( sphereMesh );
+  //
+  renderer = new THREE.WebGLRenderer();
+  renderer.autoClear = false;
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  var container = document.getElementById('scene');
+  container.appendChild( renderer.domElement );
+  renderer.gammaOutput = true;
+  //
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.minDistance = 500;
+  controls.maxDistance = 2500;
+  // Grid
+  var size = 10000;
+  var divisions = 10;
+  var colorCenterLine = 0xffffff;
+  var colorGrid = 0xffffff;
+  var gridHelper = new THREE.GridHelper( size, divisions, colorCenterLine, colorGrid );
+  scene.add( gridHelper );
+  // Axes
+  addAxes(size);
+
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  cameraCube.aspect = window.innerWidth / window.innerHeight;
+  cameraCube.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+//
+function animate() {
+  requestAnimationFrame( animate );
+  render();
+}
+
+
+function render() {
+  camera.lookAt( scene.position );
+  cameraCube.rotation.copy( camera.rotation );
+  renderer.render( sceneCube, cameraCube );
+  renderer.render( scene, camera );
+}
+
+
+// code taken from https://soledadpenades.com/articles/three-js-tutorials/drawing-the-coordinate-axes/
+function buildAxis( src, dst, colorHex, dashed ) {
+  var geom = new THREE.Geometry();
+  var mat; 
+
+  if(dashed) {
+      mat = new THREE.LineDashedMaterial({ linewidth: 5, color: colorHex, dashSize: 25, gapSize: 50 });
+  } else {
+      mat = new THREE.LineBasicMaterial({ linewidth: 5, color: colorHex });
+  }
+
+  geom.vertices.push( src.clone() );
+  geom.vertices.push( dst.clone() );
+
+  var axis = new THREE.Line( geom, mat, THREE.LineSegments );
+  axis.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+  return axis;
+
+}
+
+
+function addAxes(size) {
+  // positive X axis
+  var posX = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( size, 0, 0 ),
+    0xFF0000,
+    false
+  );
+  // negative X axis
+  var negX = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( -size, 0, 0 ),
+    0xFF0000,
+    true // ... we want this axis to be dashed
+  );
+
+  // positive Y axis
+  var posY = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 0, size, 0 ),
+    0x00FF00,
+    false
+  );
+  // negative Y axis
+  var negY = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 0, -size, 0 ),
+    0x00FF00,
+    true // ... we want this axis to be dashed
+  );
+
+  // positive Z axis
+  var posZ = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 0, 0, size ),
+    0x0000FF,
+    false
+  );
+  // negative Z axis
+  var negZ = buildAxis(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 0, 0, -size ),
+    0x0000FF,
+    true // ... we want this axis to be dashed
+  );
+
+  scene.add( posX );
+  scene.add( negX );
+  scene.add( posY );
+  scene.add( negY );
+  scene.add( posZ );
+  scene.add( negZ );
+}
