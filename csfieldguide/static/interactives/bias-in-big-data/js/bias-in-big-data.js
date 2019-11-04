636ef286d82fb767f67f8c089f051496638f8c81
courtneycb <courtneybracefield86@gmail.com>
2019-11-04 16:23:45 +1300

    Make it pretty on smaller devices.

--- csfieldguide/static/interactives/bias-in-big-data/js/bias-in-big-data.js
+++ csfieldguide/static/interactives/bias-in-big-data/js/bias-in-big-data.js
@@ -1,49 +1,69 @@
 const noUiSlider = require('nouislider');
 const wNumb = require('wnumb');
 
-const COLOURS = ['red', 'lime', 'blue', 'yellow', 'purple', 'darkorange', 'fuchsia', 'aquamarine', 'deeppink'];
+const COLOURS = ['red', 'lime', 'blue', 'yellow', 'purple', 'darkorange', 'fuchsia', 'deepskyblue'];
 const START_TEXT = gettext("Click each dot that you see on the screen, then click 'Next stage' to reveal the answer!");
-const MISSED_CIRCLES_TEXT = gettext('You seem to have missed some dots! Forced perspective like this can be used in data representation and cause bias in the overall results.');
-const SLIDER_TEXT = gettext('Click and drag the slider to change background colour. What do you notice is happening?')
+const MISSED_CIRCLES_TEXT = gettext('You seem to have missed some dots! <br><br>Forced perspective like this can be used in data representation and cause bias in the overall results.');
+const SLIDER_TEXT = gettext('Click and drag the slider to change background colour. <br><br>What do you notice is happening?')
 const SLIDER_MIN = 0;
 const SLIDER_MAX = 360;
-const PERENTAGE_BOUNDARY_UPPER = 95; // 95%
-const PERCENTAGE_ADJUSTMENT = 5; // 5%
+const PERENTAGE_BOUNDARY_UPPER = 86; // 86%
+const PERCENTAGE_ADJUSTMENT = 15; // 15%
 const PERENTAGE_BOUNDARY_LOWER = 5; // 5%
-var count = 0;
+const NUM_CIRCLES_TO_ADD = 8; // in addition to the 3 circles created that are the same colour as the background colour
+// below dictionary holds the 'H' value of the HSL colours.
+// these numbers correspond to the slider value of that colour, e.g blue is at value 240 on the slider.
+const SLIDER_COLOUR_VALUES = {
+    'red': 0,
+    'lime': 120,
+    'blue': 240,
+    'yellow': 60,
+    'purple': 277,
+    'darkorange': 33,
+    'fuchsia': 300,
+    'deepskyblue': 195
+}
 var firstStage = true;
 var bgColourSlider = $('#background-colour-slider');
+var sliderStartPos = 0;
+var startColour = 'red';
+
 
 $(document).ready(function() {
     init();
-    createSlider();
     $('#next-stage').click(loadNextStage);
     $('#start-again').click(restartInteractive);
 });
 
+
 function init() {
-    // randomly set position of 3 red circles already created.
-    $('.circle').each(function() {
-        $(this).css(getRandomPosition());
-    });
-    for (i=0; i < 8; i++) {
+    // get random background colour to start
+    startColour =  getRandomColour();
+    sliderStartPos = SLIDER_COLOUR_VALUES[startColour];
+    $('#circles-area').addClass(startColour);
+    // make sure we have at least 3 circles that are the same as the background colour
+    createCircle(startColour);
+    createCircle(startColour);
+    createCircle(startColour);
+    // generate 8 more randomly coloured circles
+    for (i=0; i < NUM_CIRCLES_TO_ADD; i++) {
         createCircle();
     }
     $('.circle').removeClass('d-none');
-    $('#count').text(count).removeClass('d-none');
     $('.circle').click(function() {
-        count += 1;
-        $('#count').text(count);
         // add glow around clicked circle
         $(this).addClass('glow');
     });
     $('#next-stage').removeClass('d-none');
     $('#start-again').addClass('d-none');
+    createSlider();
+    bgColourSlider[0].noUiSlider.on('update', updateSlider);
 }
+
 
 function createSlider() {
     noUiSlider.create(bgColourSlider[0], {
-        start: 0,
+        start: sliderStartPos,
         step: 1,
         connect: "lower",
         orientation: "horizontal",
@@ -55,13 +75,16 @@ function createSlider() {
           decimals: 0
         })
     });
-    bgColourSlider[0].noUiSlider.on('update', function() {
-        var value = bgColourSlider[0].noUiSlider.get();
-        hslColour = 'hsl(' + value + ', 100%, 50%)';
-        $('#circles-area').css('background-color', hslColour);
-        $('.noUi-handle').css('background-color', hslColour);
-    });
 }
+
+
+function updateSlider() {
+    var value = bgColourSlider[0].noUiSlider.get();
+    hslColour = 'hsl(' + value + ', 100%, 50%)';
+    $('#circles-area').css('background', hslColour);
+    $('.noUi-handle').css('background', hslColour);
+}
+
 
 function getRandomPosition() {
     var circlesAreaHeight = $('#circles-area').height();
@@ -91,40 +114,46 @@ function getRandomPosition() {
     };
 }
 
-function createCircle() {
-    var colour =  COLOURS[Math.floor(Math.random() * COLOURS.length)];
+
+function createCircle(colour) {
+    var colour =  colour || getRandomColour();
     var $circle = $("<div>").addClass('circle dynamic d-none ' + colour);
     $circle.css(getRandomPosition());
     $('#circles-area').append($circle);
 }
 
+
 function loadNextStage() {
     if (firstStage) {
         // what happens if they haven't missed any circles?
-        $('#circles-area').css('background-color', 'grey');
-        $('#instruction-text').text(MISSED_CIRCLES_TEXT);
+        $('#circles-area').removeClass(startColour);
+        $('#circles-area').addClass('grey');
+        $('#instruction-text').html(MISSED_CIRCLES_TEXT);
         firstStage = false;
     } else {
-        $('#instruction-text').text(SLIDER_TEXT);
+        $('#instruction-text').html(SLIDER_TEXT);
         $('.circle').removeClass('glow');
-        $('#circles-area').css('background-color', 'red');
+        $('#circles-area').removeClass('grey');
+        $('#circles-area').addClass(startColour);
         $('#next-stage').addClass('d-none');
         $('#start-again').removeClass('d-none');
-        bgColourSlider.removeClass('d-none');
+        $('#background-colour-slider-container').removeClass('d-none');
         // disable click event for circles
         $('.circle').off('click');
-        $('#count').addClass('d-none');
     }
 }
 
+
 function restartInteractive() {
-    $('.circle.dynamic').remove();
-    bgColourSlider.addClass('d-none');
-    $('.noUi-handle').css('background-color', 'red');
-    bgColourSlider[0].noUiSlider.set(0);
+    $('.circle').remove();
+    $('#background-colour-slider-container').addClass('d-none');
     firstStage = true;
-    count = 0;
-    $('#instruction-text').text(START_TEXT);
+    $('#instruction-text').html(START_TEXT);
+    bgColourSlider[0].noUiSlider.destroy();
     init();
-    $('#circles-area').css('background-color', 'red');
+}
+
+
+function getRandomColour() {
+    return COLOURS[Math.floor(Math.random() * COLOURS.length)];
 }
