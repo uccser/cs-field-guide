f99fbde22c65f2865c15390768ad4d540d9b86df
eAlasdair <eAlasdair@gmail.com>
2020-06-22 12:57:31 +1200

    shift lzss algorithm to helper module file

--- csfieldguide/static/interactives/lzss-compression/js/lzss-compression.js
+++ csfieldguide/static/interactives/lzss-compression/js/lzss-compression.js
@@ -1,9 +1,5 @@
-var sliding_window;
-var string_to_match;
-var num_characters = 1;
-// set the min and max match length
-var min_match_length = 2;
-var max_match_length = 16;
+const lzssAlgorithm = require('./lzssAlgorithm.js');
+
 var encoded_message = [];
 var start_index;
 var end_index;
@@ -25,7 +21,7 @@ function compress() {
     var message = document.getElementById('message-to-encode').value;
     // clear any existed encoded message
     document.getElementById('lzss-compression-compressed-text').innerHTML = '';
-    compressText(message);
+    encoded_message = lzssAlgorithm.compressText(message);
     drawEncodedMessage(encoded_message);
 }
 
@@ -172,110 +168,5 @@ function autoTab(event) {
         element.focus();
         changeHighlight(event, false);
         changeHighlight(element, true);
-    }
-}
-
-// Runs the LZSS algorithm
-function compressText(message) {
-    message = message.split('');
-
-    for (var i = 0; i < message.length; i++) {
-        message[i] = message[i].replace(/[\r\n]+/g, null);
-        message[i] = message[i].replace(/[\s]+/g, "\u2423");
-    }
-
-    // initialise sliding window and initial encoded message
-    sliding_window = message.slice(0, min_match_length);
-    encoded_message = message.slice(0, min_match_length);
-
-    message.splice(0, min_match_length);
-
-    // read in string to length of max match
-    string_to_match = message.splice(0, max_match_length);
-    while (true) {
-        if (string_to_match.length > 0) {
-
-            // STEP 3 search for longest matching string in sliding window
-            var match_offset;
-            var longest_match_offset;
-            var longest_match_length = 0;
-            var current_length_of_match = 0;
-
-            for (var i = 0; i < sliding_window.length; i++) {
-                // get next character in sliding window
-                sw_character = sliding_window[i];
-
-                if (string_to_match[0] == 'null') { // meaning is newline character
-                    // add newline character to sliding window and remove from message
-                    newline_character = string_to_match.slice(0, 1);
-                    sliding_window.push(newline_character);
-                    string_to_match.splice(0, 1);
-                    // put next chracter on string to match
-                    string_to_match.push(message.splice(0, 1)[0]);
-                    // add newline to output
-                    encoded_message.push(newline_character);
-                }
-
-                if (sw_character == string_to_match[0]) {
-                    // record the current position as the start of the match in the sw
-                    match_offset = i;
-                    current_length_of_match = 1;
-                    var next_sw_character_index = i;
-
-                    for (var j = 1; j < string_to_match.length; j++) {
-                        // work out what the next characters are    
-                        next_sw_character_index = next_sw_character_index + 1;
-                        var next_sw_character = sliding_window[next_sw_character_index];
-                        var next_search_character = string_to_match[j];
-
-                        if (next_search_character == 'null') {
-                            break
-                        }
-
-                        // if the next characters match, increase the length of the match
-                        if (next_sw_character == next_search_character) {
-                            current_length_of_match += 1;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    // work out if best match so far
-                    if (current_length_of_match > longest_match_length) {
-                        longest_match_length = current_length_of_match;
-                        longest_match_offset = match_offset;
-                    }
-                }
-            }
-
-            // STEP 4 write result to output
-            if (longest_match_length >= min_match_length) {
-                num_characters = longest_match_length;
-                var matched_characters = string_to_match.splice(0, longest_match_length);
-                for (var k = 0; k < matched_characters.length; k++) {
-                    sliding_window.push(matched_characters[k]);
-                }
-                encoded_message.push([longest_match_offset, longest_match_length]);
-            } else {
-                num_characters = 1;
-                unencoded_symbol = string_to_match.splice(0, 1)[0];
-                encoded_message.push(unencoded_symbol);
-                sliding_window.push(unencoded_symbol);
-            }
-
-            // prepare the next string to check
-            characters_to_add = message.splice(0, num_characters);
-            if (characters_to_add.length > 0) {
-                for (var l = 0; l < num_characters; l++) {
-                    next_character_to_add = characters_to_add[l];
-                    if (next_character_to_add != undefined) {
-                        string_to_match.push(characters_to_add[l]);
-                    }
-                }
-            }
-
-        } else {
-            break;
-        }
     }
 }
---
+++ csfieldguide/static/interactives/lzss-compression/js/lzssAlgorithm.js
@@ -0,0 +1,118 @@
+/**
+ * A fake lzss encoding algorithm. Not as efficient as the real thing, but gives the same result
+ */
+
+var slidingWindow;
+var stringToMatch;
+var numCharacters = 1;
+// set the min and max match length
+const MIN_LENGTH = 2;
+var MAX_LENGTH = 16;
+
+var encodedMessage = [];
+
+function compressText(message) {
+  message = message.split('');
+
+  for (var i = 0; i < message.length; i++) {
+    message[i] = message[i].replace(/[\r\n]+/g, null);
+    message[i] = message[i].replace(/[\s]+/g, "\u2423");
+  }
+
+  // initialise sliding window and initial encoded message
+  slidingWindow = message.slice(0, MIN_LENGTH);
+  encodedMessage = message.slice(0, MIN_LENGTH);
+
+  message.splice(0, MIN_LENGTH);
+
+  // read in string to length of max match
+  stringToMatch = message.splice(0, MAX_LENGTH);
+  while (stringToMatch.length > 0) {
+
+    // STEP 3 search for longest matching string in sliding window
+    var matchOffset;
+    var longestMatchOffset;
+    var longestMatchLength = 0;
+    var currentMatchLength = 0;
+
+    for (var i = 0; i < slidingWindow.length; i++) {
+      // get next character in sliding window
+      windowCharacter = slidingWindow[i];
+
+      if (stringToMatch[0] == null) { // meaning is newline character
+        // add newline character to sliding window and remove from message. TODO: Consider encoding as a normal character
+        newline_character = stringToMatch.slice(0, 1);
+        slidingWindow.push(newline_character);
+        stringToMatch.splice(0, 1);
+        // put next chracter on string to match
+        stringToMatch.push(message.splice(0, 1)[0]);
+        // add newline to output
+        encodedMessage.push(newline_character);
+      }
+
+      if (windowCharacter == stringToMatch[0]) {
+        // record the current position as the start of the match in the sw
+        matchOffset = i;
+        currentMatchLength = 1;
+        var nextWindowCharacterIndex = i;
+
+        for (var j = 1; j < stringToMatch.length; j++) {
+          // work out what the next characters are    
+          nextWindowCharacterIndex = nextWindowCharacterIndex + 1;
+          var nextWindowCharacter = slidingWindow[nextWindowCharacterIndex];
+          var nextSearchCharacter = stringToMatch[j];
+
+          if (nextSearchCharacter == 'null') {
+            break
+          }
+
+          // if the next characters match, increase the length of the match
+          if (nextWindowCharacter == nextSearchCharacter) {
+            currentMatchLength += 1;
+          } else {
+            break;
+          }
+        }
+
+        // work out if best match so far
+        if (currentMatchLength > longestMatchLength) {
+          longestMatchLength = currentMatchLength;
+          longestMatchOffset = matchOffset;
+        }
+      }
+    }
+
+    // STEP 4 write result to output
+    if (longestMatchLength >= MIN_LENGTH) {
+      numCharacters = longestMatchLength;
+      var matchedCharacters = stringToMatch.splice(0, longestMatchLength);
+      for (var k = 0; k < matchedCharacters.length; k++) {
+        slidingWindow.push(matchedCharacters[k]);
+      }
+      encodedMessage.push([longestMatchOffset, longestMatchLength]);
+    } else {
+      numCharacters = 1;
+      var unencodedSymbol = stringToMatch.splice(0, 1)[0];
+      encodedMessage.push(unencodedSymbol);
+      slidingWindow.push(unencodedSymbol);
+    }
+
+    // prepare the next string to check
+    var charactersToAdd = message.splice(0, numCharacters);
+    if (charactersToAdd.length > 0) {
+      for (var l = 0; l < numCharacters; l++) {
+        var nextCharacterToAdd = charactersToAdd[l];
+        if (nextCharacterToAdd != undefined) {
+          stringToMatch.push(nextCharacterToAdd);
+        }
+      }
+    }
+  }
+  console.log(slidingWindow);
+  console.log(encodedMessage);
+  return encodedMessage;
+}
+
+module.exports = {
+  compressText,
+}
