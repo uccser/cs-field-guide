8751e1912826b3448c04f35234ef53c199c4db4e
eAlasdair <ealasdair@gmail.com>
2021-01-27 12:13:52 +1300

    add url parameters to define the grammar

--- csfieldguide/static/interactives/cfg-equation-builder/README.md
+++ csfieldguide/static/interactives/cfg-equation-builder/README.md
@@ -4,6 +4,11 @@
 
 This interactive demonstrates a context-free grammar (CFG) by allowing a user to use it to build a mathematical equation.
 
+## URL Parameters
+
+Parameters, particularly the grammar, should have all non-unreserved characters [percent-encoded](https://en.wikipedia.org/wiki/Percent-encoding).
+Integers can only be terminal.
+
 ## Required files
 
 The interactive loads from a base website template which includes a JavaScript file containing jQuery, Bootstrap, and a few other utilities and polyfills.
--- csfieldguide/static/interactives/cfg-equation-builder/js/cfg-equation-builder.js
+++ csfieldguide/static/interactives/cfg-equation-builder/js/cfg-equation-builder.js
@@ -1,14 +1,16 @@
+var urlParameters = require('../../../js/third-party/url-parameters.js');
+
 /**
- * Productions in the grammar.
+ * Productions in the default grammar.
  * A number or a string that begins and ends with an inverted comma (') is
  * interpreted as terminal, everything else as nonterminal.
  * A production with only terminals can be a list of elements rather than a list of lists
  */
-const PRODUCTIONS = {
+const DEFAULT_PRODUCTIONS = {
   "E": [
     ["N"],
     ["E", "'+'", "E"],
-    ["E", "'\u00D7'", "E"],
+    ["E", "'*'", "E"],
     ["'-'", "E"],
     ["'('", "E", "')'"],
   ],
@@ -19,39 +21,219 @@ const PRODUCTIONS = {
  * Used when generating random expressions.
  * When the max recursion depth is reached one of these will be used.
  */
-const FINAL_TERMINALS = [0,1,2,3,4,5,6,7,8,9];
+const DEFAULT_FINAL_TERMINALS = [0,1,2,3,4,5,6,7,8,9];
 
-var $activeNonterminal = null;
-var historyStack = [];
+var $activeNonterminal_ = null;
+var historyStack_ = [];
+var productions_ = DEFAULT_PRODUCTIONS;
+var finalTerminals_ = DEFAULT_FINAL_TERMINALS;
+var initialNonterminal_ = 'E'
 
 $(document).ready(function() {
+  parseUrlParameters();
+  $('#cfg-equation').html(`<span class="nonterminal">${initialNonterminal_}</span>`);
+  fillProductionsWindow(productions_);
   $('#generate-button').on('click', function() {
-    $('#cfg-target').val(randomExpression('E', PRODUCTIONS, 3, FINAL_TERMINALS));
+    $('#cfg-target').val(randomExpression(initialNonterminal_, productions_, 3, finalTerminals_));
     testMatchingEquations();
   });
   $('#reset-button').on('click', function() {
     $('#cfg-equation').html('<span class="nonterminal">E</span>');
     reapplyNonterminalClickEvent();
     testMatchingEquations();
-    historyStack = [];
-    $("#undo-button").prop('disabled', true);
+    historyStack_ = [];
+    $('#undo-button').prop('disabled', true);
   });
-  $("#undo-button").on('click', undo);
-  $("#undo-button").prop('disabled', true);
+  $('#undo-button').on('click', undo);
+  $('#undo-button').prop('disabled', true);
   $('#cfg-target').change(testMatchingEquations);
-  $('#cfg-target').val(randomExpression('E', PRODUCTIONS, 1, FINAL_TERMINALS));
+  $('#cfg-target').val(randomExpression(initialNonterminal_, productions_, 1, finalTerminals_));
   reapplyNonterminalClickEvent();
   //https://stackoverflow.com/a/3028037
   $(document).click(function(event) { 
     var $target = $(event.target);
     if(!$target.closest('.nonterminal').length &&
     !$target.closest('#selection-popup').length &&
-    $('#selection-popup').is(":visible")) {
+    $('#selection-popup').is(':visible')) {
       $('#selection-popup').hide();
-      $activeNonterminal = null;
+      $activeNonterminal_ = null;
     }        
   });
 });
+
+/******************************************************************************/
+// FUNCTIONS FOR PARSING THE URL //
+/******************************************************************************/
+
+/**
+ * Interprets the given URL parameters and prepares the interactive accordingly
+ * 
+ * The getUrlParameter function should take care of un-escaping reserved characters
+ */
+function parseUrlParameters() {
+  var grammar = urlParameters.getUrlParameter('productions');
+  var finalTerminals = urlParameters.getUrlParameter('terminals');
+  var examples = urlParameters.getUrlParameter('examples');
+
+  if (grammar) {
+    productions_ = decodeGrammar(grammar);
+  }
+  if (finalTerminals) {
+    finalTerminals_ = decodeTerminals(finalTerminals);
+  }
+}
+
+/**
+ * Parses the given string to form a dictionary of grammar productions.
+ * 
+ * e.g. the default productions could be parsed from:
+ * E : N | E '+' E | E '*' E | '-' E | '(' E ')' ; N : '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' ;
+ */
+function decodeGrammar(productionString) {
+  var duo, nonterminal, replacementString;
+  var replacements = [];
+  var productions = {};
+  var blocks = productionString.split(";");
+  initialNonterminal_ = blocks[0].split(':')[0].trim();
+  for (var blockIndex=0; blockIndex < blocks.length; blockIndex++) {
+    if (blocks[blockIndex].trim() == '') {
+      continue;
+    }
+    duo = blocks[blockIndex].split(':');
+    if (duo.length != 2) {
+      console.error("Invalid syntax for given grammar productions: " + blocks[blockIndex]);
+      continue;
+    }
+    nonterminal = duo[0].trim();
+    replacementString = duo[1];
+    replacements = (interpretReplacementStrings(replacementString.split('|')));
+    productions[nonterminal] = replacements;
+  }
+  return productions;
+}
+
+/**
+ * Returns a list of production replacements in the format expected by this program.
+ * If every replacement is an integer, returns a list of elements rather than
+ * a list of lists
+ */
+function interpretReplacementStrings(replacementStrings) {
+  const isNumber = (value) => typeof(value) == 'number';
+  var replacements = [];
+  var replacementUnits;
+  for (var i=0; i<replacementStrings.length; i++) {
+    replacementUnits = replacementStrings[i].trim().split(' ');
+    var firstUnit = replacementUnits[0].replace(/^\'+|\'+$/g, '');
+    if (replacementUnits.length == 1 && !isNaN(firstUnit)
+    && parseInt(firstUnit) == parseFloat(firstUnit)) {
+      // A full list of integer terminals is interpreted differently
+      replacements.push(parseInt(firstUnit));
+    } else {
+      replacements.push(replacementUnits);
+    }
+  }
+  if (!replacements.every(isNumber)) {
+    // If not all of them are integers then we have to resort to the long form standard
+    for (i=0; i<replacements.length; i++) {
+      if (isNumber(replacements[i])) {
+        replacements[i] = [`'${replacements[i].toString()}'`];
+      }
+    }
+  }
+  return replacements
+}
+
+/**
+ * Parses the given string to form a list of terminal strings.
+ * With outer whitespace and inverted commas removed, everything between '|'
+ * symbols is an individual terminal.
+ * 
+ * e.g. a|b|c|d , 'a'|'b'|'c'|'d' , 'a' | 'b' | 'c' | 'd' all decode to the same thing
+ */
+function decodeTerminals(terminalString) {
+  var terminals = terminalString.split('|');
+  for (var i=0; i<terminals.length; i++) {
+    terminals[i] = terminals[i].trim().replace(/^\'+|\'+$/g, '');
+  }
+  return terminals;
+}
+
+/******************************************************************************/
+// FUNCTIONS FOR PREPARING THE INTERACTIVE //
+/******************************************************************************/
+
+/**
+ * Fills the user-facing table with the given grammar productions.
+ * If the productions are sufficiently short, they are displayed in 2 columns
+ */
+function fillProductionsWindow(productions) {
+  var keys = Object.keys(productions)
+  var describedProductions = [];
+  for (i=0; i<keys.length; i++) {
+    describedProductions = describedProductions.concat(describeAndReduceProductions(keys[i], productions[keys[i]]));
+  }
+
+  var use2Cols = describedProductions.length > 1;
+  for (var x=0; x<describedProductions.length; x++) {
+    // 14 is the length of &#8594 plus 8 characters
+    if (describedProductions[x].length > 14) {
+      use2Cols = false;
+      break;
+    }
+  }
+
+  var $table = $('#productions-table')
+  var table = "";
+  if (use2Cols) {
+    table += (`<tr><th colspan="2"><h3>${gettext("Productions:")}</h3></th></tr>`)
+    var half = Math.ceil(describedProductions.length / 2);
+    for (var i=0; i<half; i++) {
+      if (half + i < describedProductions.length) {
+        table += (`<tr><td>${describedProductions[i]}</td><td>${describedProductions[half + i]}</td></tr>`)
+      } else {
+        table += (`<tr><td>${describedProductions[i]}</td><td></td></tr>`)
+      }
+    }
+  } else {
+    table += (`<tr><th><h3>${gettext("Productions:")}</h3></th></tr>`)
+    for (var j=0; j<describedProductions.length; j++) {
+      table += (`<tr><td>${describedProductions[j]}</td></tr>`)
+    }
+  }
+  $table.html(table);
+}
+
+/**
+ * Returns a list of strings, each describing productions from a given nonterminal
+ * 
+ * If replacements is an incremental list of integers they are reduced appropriately
+ */
+function describeAndReduceProductions(nonterminal, replacements) {
+  var isCompressable = true;
+  if (typeof(replacements[0]) == 'number') {
+    for (var i=1; i<replacements.length; i++) {
+      if (replacements[i] != replacements[i-1] + 1) {
+        isCompressable = false;
+        break;
+      }
+    }
+  } else {
+    isCompressable = false;
+  }
+
+  if (isCompressable) {
+    return [`${nonterminal} &#8594 ${replacements[0]}-${replacements[replacements.length - 1]}`]
+  }
+  var returnList = [];
+  for (i=0; i<replacements.length; i++) {
+    returnList.push(describeProduction(nonterminal, replacements[i]));
+  }
+  return returnList;
+}
+
+/******************************************************************************/
+// FUNCTIONS FOR INTERACTIVE LOGIC //
+/******************************************************************************/
 
 /**
  * Each time a new nonterminal is created it needs to be bound to the click event.
@@ -61,9 +243,9 @@ function reapplyNonterminalClickEvent() {
   //https://stackoverflow.com/a/44753671
   $('.nonterminal').on('click', function(event) {
     setActiveNonterminal($(event.target));
-    $("#selection-popup").css({left: event.pageX});
-    $("#selection-popup").css({top: event.pageY});
-    $("#selection-popup").show();
+    $('#selection-popup').css({left: event.pageX});
+    $('#selection-popup').css({top: event.pageY});
+    $('#selection-popup').show();
   });
 }
 
@@ -73,12 +255,12 @@ function reapplyNonterminalClickEvent() {
  * If they match, applies an effect, else removes it.
  */
 function testMatchingEquations() {
-  if ($("#cfg-target").val().trim() == $("#cfg-equation").html().trim()) {
-    $("#cfg-equation").addClass("success");
-    $("#generate-button").addClass("success");
+  if ($('#cfg-target').val().trim() == $('#cfg-equation').html().trim()) {
+    $('#cfg-equation').addClass('success');
+    $('#generate-button').addClass('success');
   } else {
-    $("#cfg-equation").removeClass("success");
-    $("#generate-button").removeClass("success");
+    $('#cfg-equation').removeClass('success');
+    $('#generate-button').removeClass('success');
   }
 }
 
@@ -87,15 +269,15 @@ function testMatchingEquations() {
  * Prepares the popup appropriately.
  */
 function setActiveNonterminal($target) {
-  $activeNonterminal = $target;
+  $activeNonterminal_ = $target;
   var nonterminal = $target.html();
   $('#selection-popup').html('');
-  if (Object.keys(PRODUCTIONS).indexOf(nonterminal) < 0) {
+  if (Object.keys(productions_).indexOf(nonterminal) < 0) {
     console.error(`Could not find nonterminal ${nonterminal} in available productions.`);
     $('#selection-popup').html('No replacements available.');
     return;
   }
-  $('#selection-popup').html(getPopupVal(nonterminal, PRODUCTIONS[nonterminal]));
+  $('#selection-popup').html(getPopupVal(nonterminal, productions_[nonterminal]));
   $('.cfg-popup-replacement').on('click', function(event) {
     applyProduction($(event.target));
     $('#selection-popup').hide();
@@ -107,14 +289,14 @@ function setActiveNonterminal($target) {
  * Replaces the active nonterminal using the target production.
  */
 function applyProduction($target) {
-  var nonterminal = $activeNonterminal.html();
+  var nonterminal = $activeNonterminal_.html();
   var replacementIndex = parseInt($target.attr('cfg-replacement'));
-  var replacement = PRODUCTIONS[nonterminal][replacementIndex];
-  historyStack.push($("#cfg-equation").html());
-  $("#undo-button").prop('disabled', false);
-  $activeNonterminal.replaceWith(describeProductionReplacement(replacement));
+  var replacement = productions_[nonterminal][replacementIndex];
+  historyStack_.push($('#cfg-equation').html());
+  $('#undo-button').prop('disabled', false);
+  $activeNonterminal_.replaceWith(describeProductionReplacement(replacement));
   reapplyNonterminalClickEvent();
-  $activeNonterminal = null;
+  $activeNonterminal_ = null;
 }
 
 /**
@@ -122,10 +304,10 @@ function applyProduction($target) {
  * Only one previous version is stored.
  */
 function undo() {
-  $("#cfg-equation").html(historyStack.pop());
-  $activeNonterminal = null;
-  if (historyStack.length <= 0) {
-    $("#undo-button").prop('disabled', true);
+  $('#cfg-equation').html(historyStack_.pop());
+  $activeNonterminal_ = null;
+  if (historyStack_.length <= 0) {
+    $('#undo-button').prop('disabled', true);
   }
   reapplyNonterminalClickEvent();
   testMatchingEquations();
@@ -182,6 +364,10 @@ function describeProductionReplacement(replacement) {
   }
   return code;
 }
+
+/******************************************************************************/
+// FUNCTIONS FOR CREATING RANDOM EQUATIONS //
+/******************************************************************************/
 
 /**
  * Returns a random integer in the range [0, max).
