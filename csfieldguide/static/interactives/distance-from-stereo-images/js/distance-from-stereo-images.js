574bb47e3c370acf99d10cf67cb5a4633eaca787
Jackie Qiu <jqi26@uclive.ac.nz>
2021-04-13 17:36:17 +1200

    Clicking the canvas now adds a dot. Canvas adapts to resizing as well. However, scrolling affects the click coordinates as I am still figuring out how to get the scroll amount.

--- csfieldguide/static/interactives/distance-from-stereo-images/js/distance-from-stereo-images.js
+++ csfieldguide/static/interactives/distance-from-stereo-images/js/distance-from-stereo-images.js
@@ -1,7 +1,14 @@
-let leftImage = null
-let rightImage = null
-let leftCanvas = null
-let rightCanvas = null
+let leftImage = null;
+let rightImage = null;
+let leftCanvas = null;
+let rightCanvas = null;
+
+let leftX = null;
+let leftY = null;
+let rightX = null;
+let rightY = null;
+
+const DOT_SIZE = 10;
 
 $(document).ready(function () {
     $('#stereo-left-input').change(loadLeftImageDialog);
@@ -16,16 +23,26 @@ $(document).ready(function () {
 
   updateLeftCanvas();
   updateRightCanvas()
+
+  window.onresize = function() {
+    updateLeftCanvas();
+    updateRightCanvas();
+    leftX = leftY = rightX = rightY = null;
+    document.getElementById("left-x").innerHTML = null;
+    document.getElementById("left-y").innerHTML = null;
+    document.getElementById("right-x").innerHTML = null;
+    document.getElementById("right-y").innerHTML = null;
+  }
 });
 
 function updateLeftCanvas() {
-  leftCanvas.width = leftImage.clientWidth;
-  leftCanvas.height = leftImage.clientHeight;
+  leftCanvas.width = leftImage.width;
+  leftCanvas.height = leftImage.height;
 }
 
 function updateRightCanvas() {
-  rightCanvas.width = rightImage.clientWidth;
-  rightCanvas.height = rightImage.clientHeight;
+  rightCanvas.width = rightImage.width;
+  rightCanvas.height = rightImage.height;
 }
 
 /*
@@ -38,6 +55,10 @@ function loadLeftImageDialog() {
     reader.onload = function (e) {
       leftImage.crossOrigin = 'anonymous';
       leftImage.src = e.target.result;
+      // Sometimes the image seemingly is not ready yet, meaning the canvas won't update to the new size. A delay is
+      // created to reduce the chances of this occurring, though it still does happen very rarely. Increasing the
+      // timeout (or finding a better solution) may fix this.
+      setTimeout(() => {  updateLeftCanvas(); }, 10);
     }
     reader.readAsDataURL(input.files[0]);
     $("label[for='stereo-left-input']").text(input.files[0].name);
@@ -54,72 +75,83 @@ function loadRightImageDialog() {
     reader.onload = function (e) {
       rightImage.crossOrigin = 'anonymous';
       rightImage.src = e.target.result;
+      setTimeout(() => {  updateRightCanvas(); }, 10);
     }
     reader.readAsDataURL(input.files[0]);
     $("label[for='stereo-right-input']").text(input.files[0].name);
   }
 }
 
+//*
+// =====================================================================================================================
+// Code to get coordinates from image adapted from https://www.chestysoft.com/imagefile/javascript/get-coordinates.asp
+// =====================================================================================================================
+//*
 function findPosition(image)
 {
-  if(typeof( image.offsetParent ) != "undefined")
-  {
-    for(var posX = 0, posY = 0; image; image = image.offsetParent)
-    {
+  if (typeof( image.offsetParent ) != "undefined") {
+    for(let posX = 0, posY = 0; image; image = image.offsetParent) {
       posX += image.offsetLeft;
       posY += image.offsetTop;
     }
     return [ posX, posY ];
-  }
-  else
-  {
+  } else {
     return [ image.x, image.y ];
   }
 }
 
 function getLeftCoordinates(event)
 {
-  let posX = 0;
-  let posY = 0;
   let imgPos = findPosition(leftCanvas);
   if (event.pageX || event.pageY)
   {
-    posX = event.pageX;
-    posY = event.pageY;
+    leftX = event.pageX;
+    leftY = event.pageY;
   }
   else if (event.clientX || event.clientY)
   {
-    posX = event.clientX + document.body.scrollLeft
+    leftX = event.clientX + document.body.scrollLeft
       + document.documentElement.scrollLeft;
-    posY = event.clientY + document.body.scrollTop
+    leftY = event.clientY + document.body.scrollTop
       + document.documentElement.scrollTop;
   }
-  posX = posX - imgPos[0];
-  posY = posY - imgPos[1];
-  document.getElementById("left-x").innerHTML = posX;
-  document.getElementById("left-y").innerHTML = posY;
-  drawDot(posX, posY)
+  leftX = leftX - imgPos[0];
+  leftY = leftY - imgPos[1];
+  document.getElementById("left-x").innerHTML = leftX;
+  document.getElementById("left-y").innerHTML = leftY;
+  drawDot(leftX, leftY, leftCanvas);
 }
 
 function getRightCoordinates(event)
 {
-  let posX = 0;
-  let posY = 0;
   let imgPos = findPosition(rightCanvas);
   if (event.pageX || event.pageY)
   {
-    posX = event.pageX;
-    posY = event.pageY;
+    rightX = event.pageX;
+    rightY = event.pageY;
   }
   else if (event.clientX || event.clientY)
   {
-    posX = event.clientX + document.body.scrollLeft
+    rightX = event.clientX + document.body.scrollLeft
       + document.documentElement.scrollLeft;
-    posY = event.clientY + document.body.scrollTop
+    rightY = event.clientY + document.body.scrollTop
       + document.documentElement.scrollTop;
   }
-  posX = posX - imgPos[0];
-  posY = posY - imgPos[1];
-  document.getElementById("right-x").innerHTML = posX;
-  document.getElementById("right-y").innerHTML = posY;
+  rightX = rightX - imgPos[0];
+  rightY = rightY - imgPos[1];
+  document.getElementById("right-x").innerHTML = rightX;
+  document.getElementById("right-y").innerHTML = rightY;
+  drawDot(rightX, rightY, rightCanvas);
+}
+//*
+// =====================================================================================================================
+// End of adapted code.
+// =====================================================================================================================
+//*
+
+function drawDot(x, y, canvas) {
+  let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(x - 0.5 * DOT_SIZE, y - 0.5 * DOT_SIZE, DOT_SIZE, DOT_SIZE);
 }
