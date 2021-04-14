713e811ca9bb8e58d0abe02382e7726799ed2970
Jackie Qiu <jqi26@uclive.ac.nz>
2021-04-14 16:56:17 +1200

    Used an alternative method for getting the click coordinates to avoid issues with scrolling.

--- csfieldguide/static/interactives/distance-from-stereo-images/js/distance-from-stereo-images.js
+++ csfieldguide/static/interactives/distance-from-stereo-images/js/distance-from-stereo-images.js
@@ -11,15 +11,15 @@ let rightY = null;
 const DOT_SIZE = 10;
 
 $(document).ready(function () {
-    $('#stereo-left-input').change(loadLeftImageDialog);
-    $('#stereo-right-input').change(loadRightImageDialog);
+  $('#stereo-left-input').change(loadLeftImageDialog);
+  $('#stereo-right-input').change(loadRightImageDialog);
 
   leftImage = document.getElementById("img-left");
   leftCanvas = document.getElementById("canvas-left");
-  leftCanvas.onmousedown = getLeftCoordinates;
+  leftCanvas.onmousedown = leftCanvasClickHandler;
   rightImage = document.getElementById("img-right");
   rightCanvas = document.getElementById("canvas-right");
-  rightCanvas.onmousedown = getRightCoordinates;
+  rightCanvas.onmousedown = rightCanvasClickHandler;
 
   updateLeftCanvas();
   updateRightCanvas()
@@ -55,6 +55,9 @@ function loadLeftImageDialog() {
     reader.onload = function (e) {
       leftImage.crossOrigin = 'anonymous';
       leftImage.src = e.target.result;
+      leftX = leftY = null;
+      document.getElementById("left-x").innerHTML = null;
+      document.getElementById("left-y").innerHTML = null;
       // Sometimes the image seemingly is not ready yet, meaning the canvas won't update to the new size. A delay is
       // created to reduce the chances of this occurring, though it still does happen very rarely. Increasing the
       // timeout (or finding a better solution) may fix this.
@@ -75,6 +78,9 @@ function loadRightImageDialog() {
     reader.onload = function (e) {
       rightImage.crossOrigin = 'anonymous';
       rightImage.src = e.target.result;
+      rightX = rightY = null;
+      document.getElementById("right-x").innerHTML = null;
+      document.getElementById("right-y").innerHTML = null;
       setTimeout(() => {  updateRightCanvas(); }, 10);
     }
     reader.readAsDataURL(input.files[0]);
@@ -82,72 +88,23 @@ function loadRightImageDialog() {
   }
 }
 
-//*
-// =====================================================================================================================
-// Code to get coordinates from image adapted from https://www.chestysoft.com/imagefile/javascript/get-coordinates.asp
-// =====================================================================================================================
-//*
-function findPosition(image)
-{
-  if (typeof( image.offsetParent ) != "undefined") {
-    for(let posX = 0, posY = 0; image; image = image.offsetParent) {
-      posX += image.offsetLeft;
-      posY += image.offsetTop;
-    }
-    return [ posX, posY ];
-  } else {
-    return [ image.x, image.y ];
-  }
+function leftCanvasClickHandler(event) {
+  const rect = leftCanvas.getBoundingClientRect();
+  const x = Math.round(event.clientX - rect.left);
+  const y = Math.round(event.clientY - rect.top);
+  document.getElementById("left-x").innerHTML = x.toString();
+  document.getElementById("left-y").innerHTML = y.toString();
+  drawDot(x, y, leftCanvas);
 }
 
-function getLeftCoordinates(event)
-{
-  let imgPos = findPosition(leftCanvas);
-  if (event.pageX || event.pageY)
-  {
-    leftX = event.pageX;
-    leftY = event.pageY;
-  }
-  else if (event.clientX || event.clientY)
-  {
-    leftX = event.clientX + document.body.scrollLeft
-      + document.documentElement.scrollLeft;
-    leftY = event.clientY + document.body.scrollTop
-      + document.documentElement.scrollTop;
-  }
-  leftX = leftX - imgPos[0];
-  leftY = leftY - imgPos[1];
-  document.getElementById("left-x").innerHTML = leftX;
-  document.getElementById("left-y").innerHTML = leftY;
-  drawDot(leftX, leftY, leftCanvas);
+function rightCanvasClickHandler(event) {
+  const rect = rightCanvas.getBoundingClientRect();
+  const x = Math.round(event.clientX - rect.left);
+  const y = Math.round(event.clientY - rect.top);
+  document.getElementById("right-x").innerHTML = x.toString();
+  document.getElementById("right-y").innerHTML = y.toString();
+  drawDot(x, y, rightCanvas);
 }
-
-function getRightCoordinates(event)
-{
-  let imgPos = findPosition(rightCanvas);
-  if (event.pageX || event.pageY)
-  {
-    rightX = event.pageX;
-    rightY = event.pageY;
-  }
-  else if (event.clientX || event.clientY)
-  {
-    rightX = event.clientX + document.body.scrollLeft
-      + document.documentElement.scrollLeft;
-    rightY = event.clientY + document.body.scrollTop
-      + document.documentElement.scrollTop;
-  }
-  rightX = rightX - imgPos[0];
-  rightY = rightY - imgPos[1];
-  document.getElementById("right-x").innerHTML = rightX;
-  document.getElementById("right-y").innerHTML = rightY;
-  drawDot(rightX, rightY, rightCanvas);
-}
-//*
-// =====================================================================================================================
-// End of adapted code.
-// =====================================================================================================================
-//*
 
 function drawDot(x, y, canvas) {
   let ctx = canvas.getContext("2d");
