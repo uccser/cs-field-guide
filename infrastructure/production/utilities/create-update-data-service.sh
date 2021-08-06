0930eb1820e91c573791173956fdc759617f1f05
Jack Morgan <jackmorgannz@gmail.com>
2021-08-06 14:26:46 +1200

    Add missing config for Docker job

--- infrastructure/production/utilities/create-update-data-service.sh
+++ infrastructure/production/utilities/create-update-data-service.sh
@@ -10,10 +10,12 @@ docker service create \
     --constraint node.labels.role==apps \
     --env POSTGRES_HOST="postgres" \
     --env=POSTGRES_PORT="5432" \
+    --env=DEPLOYMENT_ENVIRONMENT_FILE="/cs-field-guide_deployment_environment" \
     --env=DJANGO_SECRET_KEY_FILE="/run/secrets/cs-field-guide_django_secret_key" \
     --env=POSTGRES_DB_FILE="/run/secrets/cs-field-guide_postgres_db" \
     --env=POSTGRES_USER_FILE="/run/secrets/cs-field-guide_postgres_user" \
     --env=POSTGRES_PASSWORD_FILE="/run/secrets/cs-field-guide_postgres_password" \
+    --config cs-field-guide_deployment_environment \
     --secret cs-field-guide_django_secret_key \
     --secret cs-field-guide_postgres_db \
     --secret cs-field-guide_postgres_user \
