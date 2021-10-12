c9c4df46b13b2e510a695c40fab7acb0d810303e
Henry <henry.hickmin@gmail.com>
2021-10-12 14:43:46 +1300

    Remove br from password variants. Removed unneeded text.

--- csfieldguide/static/interactives/password-strength-dict/js/password-strength-dict.js
+++ csfieldguide/static/interactives/password-strength-dict/js/password-strength-dict.js
@@ -1,32 +1,33 @@
+newText = [];
+
 $('#crackButton').click(function() {
 	crack();
 });
 
-function clear() {
-	document.getElementById("timeToCrack").innerHTML = "";
-}
+window.onload = function() {
+  var request = new XMLHttpRequest();
+  request.open('get', words_file_location, 'false');
+  request.onreadystatechange = function ()
+  {
+    if(request.readyState === 4)
+    {
+      if(request.status === 200 || request.status == 0)
+      {
+        var allText = request.responseText;
+        newText = allText.split(/\r?\n/);
+	  }
+    }
+  }
+  request.send(null);
+};
 
 
 function crack() {
-	var request = new XMLHttpRequest();
-	request.open('get', words_file_location, 'false');
-	request.onreadystatechange = function ()
-    {
-        if(request.readyState === 4)
-        {
-            if(request.status === 200 || request.status == 0)
-            {
-                var allText = request.responseText;
-                var newText = allText.split(/\r?\n/);
-                var input = document.getElementById("psw").value;
-                if (newText.includes(input)) {
-					document.getElementById("timeToCrack").innerHTML = "Cracked";
-				}	
-				else {
-					document.getElementById("timeToCrack").innerHTML = "This password isn't in our dictionary";
-				}
-            }
-        }
-    }
-    request.send(null);
+  var input = document.getElementById("psw").value;
+  if (newText.includes(input)) {
+    document.getElementById("timeToCrack").innerHTML = "Cracked";
+  }	
+  else {
+	document.getElementById("timeToCrack").innerHTML = "This password isn't in our dictionary";
+  }
 }
