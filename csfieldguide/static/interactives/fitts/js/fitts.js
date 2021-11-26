51976caf9587dd7e5d6ea168d2ace8d49f1aa9a6
Jackie Qiu <jqi26@uclive.ac.nz>
2021-11-26 16:22:11 +1300

    Altered the table to group clicks into trials and allowed the game to be restarted.
    - fitts.html: Removed placeholder numbers in click columns. Added a play again button. Expanded the table to have more functionality.
    - fitts.js: Updated so the game can be replayed. Reworked how the results table is populated. Now uses a datatable for extra functionality.

--- csfieldguide/static/interactives/fitts/js/fitts.js
+++ csfieldguide/static/interactives/fitts/js/fitts.js
@@ -8,27 +8,51 @@ let cols = [];
 let colsToWidths = [];
 let currentColToWidth = null;
 let results = [];
-let trialNum = 1;
-
+let trialNum = 0;
+let playAgainDiv = null;
+let table = null;
 
 $( document ).ready(function() {
   numCols = $('.col-sm').length;
   cols = $(".col-sm").map(function() { return this; }).get();
-  for (let col = 0; col < Math.floor(numCols / 2); col++) {
-    for (let width = 1; width <= 3; width++) {
-      colsToWidths.push({ "col": col, "width": width })
+  playAgainDiv = document.getElementById("play-again-div");
+  table = $('#results-table').DataTable( {
+    "paging":   false,
+    "info":     false,
+    "searching": false,
+    "oLanguage": {
+      "sEmptyTable": "Complete the experiment to show statistics"
     }
-  }
+  });
 
   for (let col of cols) {
     $(col).click(clickHandler);
   }
 
+  $('#play-again').click(reset);
+  reset();
+});
+
+
+function reset() {
+  playAgainDiv.hidden = true;
+  table.clear().draw();
+  results = [];
+  trialNum = 0;
+
+  for (let col = 0; col < Math.floor(numCols / 2); col++) {
+    for (let width = 1; width <= 3; width++) {
+      colsToWidths.push({ "col": col, "width": width })
+    }
+  }
+  for (let i = 0; i < colsToWidths.length; i++) {
+    results.push([]);
+  }
   currentColToWidth = getRandomItemFromList(colsToWidths);
   colsToWidths.splice(colsToWidths.indexOf(currentColToWidth), 1);
   setColumns(currentColToWidth["col"], currentColToWidth["width"]);
   start = new Date().getTime();
-});
+}
 
 
 function clickHandler(event) {
@@ -37,7 +61,7 @@ function clickHandler(event) {
     let time = end - start;
     let width = this.offsetWidth;
     let distance = Math.abs((this.offsetLeft + this.offsetWidth / 2) - lastMousePosition);
-    results.push({ "trial": trialNum, "distance": distance, "time": time, "width": width });
+    results[trialNum].push({ "distance": distance, "time": time, "width": width });
 
     lastMousePosition = event.pageX;
 
@@ -45,13 +69,13 @@ function clickHandler(event) {
     if (count === MAX_COUNT) {
       currentColToWidth = getRandomItemFromList(colsToWidths);
 
-      if (currentColToWidth == null) {
-        showResults();
-      }
-
-      colsToWidths.splice(colsToWidths.indexOf(currentColToWidth), 1);
       count = 0;
       trialNum++;
+      if (currentColToWidth == null) {
+        showResults();
+        return;
+      }
+      colsToWidths.splice(colsToWidths.indexOf(currentColToWidth), 1);
     }
     setColumns(currentColToWidth["col"], currentColToWidth["width"]);
     count++;
@@ -86,13 +110,25 @@ function getRandomItemFromList(list) {
 
 
 function showResults() {
-  let tableBody = document.getElementById("results-body");
-  for (let result of results) {
-    let row = tableBody.insertRow()
-    let [trial, distance, width, time] = [result["trial"], result["distance"], result["width"], result["time"]]
-    row.insertCell(0).innerText = trial;
-    row.insertCell(1).innerText = distance;
-    row.insertCell(2).innerText = width;
-    row.insertCell(3).innerText = time;
+  playAgainDiv.hidden = false;
+
+  for (let i = 0; i < results.length; i++) {
+    let trial = results[i];
+    trial.shift();
+    let averageDistance = Math.round(trial.reduce((partial_sum, result) => partial_sum + result["distance"], 0)
+      / trial.length * 100)  / 100;
+    let averageTime = Math.round(trial.reduce((partial_sum, result) => partial_sum + result["time"], 0)
+      / trial.length * 100) / 100;
+    let width = trial[0]["width"];
+    let trialNum = i + 1;
+    let indexOfDifficulty = Math.round(Math.log2(averageDistance / width + 1) * 100) / 100;
+
+    table.row.add([
+      trialNum,
+      averageDistance,
+      width,
+      indexOfDifficulty,
+      averageTime
+    ]).draw( false );
   }
 }
