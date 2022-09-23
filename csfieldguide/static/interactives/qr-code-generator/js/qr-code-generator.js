8c93979b65663082cdca588a0e8f3459d2b830eb
Jack Morgan <jackmorgannz@gmail.com>
2022-09-23 16:17:02 +1200

    Add ability to modify QR code

--- csfieldguide/static/interactives/qr-code-generator/js/qr-code-generator.js
+++ csfieldguide/static/interactives/qr-code-generator/js/qr-code-generator.js
@@ -1,18 +1,35 @@
 const qrcode = require('qrcode-generator');
 
-var qrCodeElement;
-var textContentInput;
-var correctionLevelSelect;
+// Elements
+var elementQRCodeContainer;
+var elementTextContentInput;
+var elementCorrectionLevelSelect;
+var elementResetButton;
+var elementHighlightChangedButton;
+var statisticsTotalBits;
+var statisticsChangedBits;
+
+// Statistic tracking
+const BIT_MODIFIED_CLASS = 'inverted';
+const SHOW_MODIFIED_BITS_CLASS = 'show-changed';
+var totalBits = 0;
+var totalChangedBits = 0;
 
 
 function setup() {
-    qrCodeElement = document.getElementById('qr-code-table');
-    textContentInput = document.getElementById('qr-code-text-content');
-    correctionLevelSelect = document.getElementById('qr-code-correction-level');
+    elementQRCodeContainer = document.getElementById('qr-code-container');
+    elementTextContentInput = document.getElementById('qr-code-text-content');
+    elementCorrectionLevelSelect = document.getElementById('qr-code-correction-level');
+    elementResetButton = document.getElementById('reset-button');
+    elementHighlightChangedButton = document.getElementById('highlight-changed-button');
+    statisticsTotalBits = document.getElementById('statistics-total-bits');
+    statisticsChangedBits = document.getElementById('statistics-changed-bits');
 
     // Setup event listeners
-    textContentInput.addEventListener('input', createQRCode);
-    correctionLevelSelect.addEventListener('change', createQRCode);
+    elementTextContentInput.addEventListener('input', createQRCode);
+    elementCorrectionLevelSelect.addEventListener('change', createQRCode);
+    elementResetButton.addEventListener('click', createQRCode);
+    elementHighlightChangedButton.addEventListener('click', function () { highlightChangedBits() });
 
     processURLParameters();
     createQRCode();
@@ -22,23 +39,74 @@ function setup() {
 function processURLParameters() {
     let searchParameters = new URL(window.location.href).searchParams;
     if (searchParameters.has('text')) {
-        textContentInput.value = searchParameters.get('text');
+        elementTextContentInput.value = searchParameters.get('text');
     }
     if (searchParameters.has('level')) {
         let providedLevel = searchParameters.get('level');
-        let validLevels = [...correctionLevelSelect.options].map(option => option.value)
+        let validLevels = [...elementCorrectionLevelSelect.options].map(option => option.value)
         if (validLevels.includes(providedLevel)) {
-            correctionLevelSelect.value = providedLevel;
+            elementCorrectionLevelSelect.value = providedLevel;
         }
     }
 }
 
 
 function createQRCode() {
-    var qr = qrcode(0, correctionLevelSelect.value);  // Auto type
-    qr.addData(textContentInput.value);
+    var qr = qrcode(0, elementCorrectionLevelSelect.value);  // 0 = Auto QR type
+    qr.addData(elementTextContentInput.value);
     qr.make();
-    qrCodeElement.innerHTML = qr.createTableTag(20, 0);
+
+    // Create QR using <div> elements
+    elementQRCodeContainer.innerHTML = '';
+    let qrSize = qr.getModuleCount();
+    for (let rowIndex = 0; rowIndex < qrSize; rowIndex++) {
+        let elementRowDiv = document.createElement('div');
+        for (let columnIndex = 0; columnIndex < qrSize; columnIndex++) {
+            let elementCellDiv = document.createElement('div');
+            elementCellDiv.classList.add('qr-code-cell');
+            if (qr.isDark(rowIndex, columnIndex)) {
+                elementCellDiv.classList.add('qr-code-cell-black');
+            } else {
+                elementCellDiv.classList.add('qr-code-cell-white');
+            }
+            elementRowDiv.appendChild(elementCellDiv);
+        }
+        elementQRCodeContainer.appendChild(elementRowDiv);
+    }
+
+    qrCodeTable = document.querySelector('#qr-code-table table');
+    totalBits = qrSize * qrSize;
+    totalChangedBits = 0;
+    highlightChangedBits(false);
+    updateStatistics();
+    elementQRCodeContainer.addEventListener('click', qrCellClicked);
+}
+
+
+function qrCellClicked(event) {
+    let cell = event.target;
+    if (cell.classList.toggle(BIT_MODIFIED_CLASS)) {
+        totalChangedBits++;
+    } else {
+        totalChangedBits--;
+    }
+    updateStatistics();
+}
+
+
+function updateStatistics() {
+    statisticsTotalBits.textContent = totalBits;
+    let percentageChanged = totalChangedBits / totalBits;
+    statisticsChangedBits.textContent = `${totalChangedBits} (${Math.round(percentageChanged * 10000) / 100}%)`;
+}
+
+
+function highlightChangedBits(force) {
+    if (elementQRCodeContainer.classList.toggle(SHOW_MODIFIED_BITS_CLASS, force)) {
+        elementHighlightChangedButton.textContent = gettext('Hide changed bits');
+    } else {
+        elementHighlightChangedButton.textContent = gettext('Show changed bits');
+    }
 }
 
 
