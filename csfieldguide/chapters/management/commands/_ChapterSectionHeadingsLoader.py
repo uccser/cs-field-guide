18397e8822b65c164a45051841b8043de36ea7fa
Philip Dolbel <pdo66@uclive.ac.nz>
2022-11-25 13:38:21 +1300

    Fix loader to handle translations

--- csfieldguide/chapters/management/commands/_ChapterSectionHeadingsLoader.py
+++ csfieldguide/chapters/management/commands/_ChapterSectionHeadingsLoader.py
@@ -39,14 +39,16 @@ class ChapterSectionHeadingsLoader(TranslatableModelLoader):
 
         """
         for language, content in self.content_translations.items():
+            i = 0
             if content.heading_tree:
-                for (i, heading_node) in enumerate(content.heading_tree):
+                for heading_node in content.heading_tree:
                     self.chapter_section.headings.update_or_create(
                         number=i,
+                        language=language,
                         defaults={
                             'name': heading_node.title,
-                            'language': language,
                             'slug': heading_node.title_slug
                         }
                     )
-        self.chapter_section.headings.filter(number__gt=i).delete()
+                    i += 1
+            self.chapter_section.headings.filter(number__gte=i, language=language).delete()
